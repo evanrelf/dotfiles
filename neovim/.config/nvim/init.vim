12ddb2604e2659ccbb51105a25a9fc0b2803c5ba
Evan Relf <evan@evanrelf.com>
2019-01-26 22:06:49 -0800

    Add Tabs layout to XMonad

--- neovim/.config/nvim/init.vim
+++ neovim/.config/nvim/init.vim
@@ -17,7 +17,7 @@ Plug 'wellle/targets.vim'
 " Plug 'michaeljsmith/vim-indent-object'
 
 " Completion
-Plug 'zxqfl/tabnine-vim'
+" Plug 'zxqfl/tabnine-vim'
 " Plug 'jiangmiao/auto-pairs'
 Plug 'alvan/vim-closetag'
 Plug 'tpope/vim-endwise'
--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,4 +1,5 @@
 import Data.Function ((&))
+import System.Exit (ExitCode(..), exitWith)
 import XMonad
 import XMonad.Actions.CycleWS (Direction1D(..), WSType(..), moveTo, shiftTo, toggleWS')
 import XMonad.Actions.FlexibleResize (mouseResizeEdgeWindow)
@@ -6,23 +7,38 @@ import XMonad.Actions.UpdateFocus (adjustEventInput, focusOnMouseMove)
 import XMonad.Actions.UpdatePointer (updatePointer)
 import qualified XMonad.Hooks.DynamicLog as DL
 import XMonad.Hooks.EwmhDesktops (ewmh, fullscreenEventHook)
+import XMonad.Hooks.InsertPosition (Focus(..), Position(..), insertPosition)
 import XMonad.Hooks.ManageDocks (avoidStruts, docks, manageDocks)
+import qualified XMonad.Layout.Decoration as Deco
 import XMonad.Layout.NoBorders (smartBorders)
 import XMonad.Layout.Renamed (Rename(..), renamed)
 import XMonad.Layout.ResizableTile (MirrorResize(..), ResizableTall(..))
 import XMonad.Layout.Spacing (Border(..), spacingRaw)
+import XMonad.Layout.Tabbed (shrinkText, tabbed)
 import XMonad.Layout.ToggleLayouts (ToggleLayout(..), toggleLayouts)
+import qualified XMonad.Prompt as Prompt
+import XMonad.Prompt.ConfirmPrompt (confirmPrompt)
 import XMonad.StackSet (RationalRect(..))
 import XMonad.Util.EZConfig (additionalKeysP, additionalMouseBindings, checkKeymap)
+import XMonad.Util.Loggers (logCmd)
 import XMonad.Util.Run (safeSpawn)
 import XMonad.Util.Scratchpad (scratchpadManageHook, scratchpadSpawnActionCustom)
+import qualified XMonad.Util.Themes as Themes
 
--- import XMonad.Hooks.InsertPosition (Focus(..), Position(..), insertPosition)
-import XMonad.Util.Loggers as Log
+-- FEATURES:
+-- * Vertically resize window (M--, M-=)
+-- * Maximize window (M-f)
+-- * Toggle scratchpad (M-s)
+-- * Tabbed layout
+-- * Conditional borders and gaps
+-- * Mouse resizing from all corners/sides
+-- * Confirm exit
+-- * etc.
 
 -- TODO:
 -- * Disable mouse snapping when resizing window with mouse
 -- * Disable/exit Full layout when only one window
+-- * Format xmobar (brightness, volume, battery, wifi, date, time)
 
 main :: IO ()
 main = xmobar myConfig >>= xmonad
@@ -30,15 +46,11 @@ main = xmobar myConfig >>= xmonad
 xmobar = DL.statusBar "xmobar" pp toggleStrutsKey
   where
     toggleStrutsKey XConfig{modMask = modm} = (modm, xK_b )
-    pp =
-      let battery = "echo -n $(acpi | cut -d ' ' -f 4-5 | sed 's/, 0\\?/ (/g' | cut -d ':' -f 1-2)')'"
-          dateTime = "date '+%a %b %d %I:%M %p' | sed 's/ 0/ /g'"
-      in DL.xmobarPP
-          { DL.ppHidden = (\w -> if w == "NSP" then "" else w)
-          , DL.ppCurrent = DL.xmobarColor "white" "" . DL.wrap "[" "]"
-          , DL.ppTitle = const "" -- DL.xmobarColor "white" "" . DL.shorten 40
-          -- , DL.ppExtras = Log.logCmd <$> [battery, dateTime]
-          }
+    pp = DL.xmobarPP
+      { DL.ppHidden = (\w -> if w == "NSP" then "" else w)
+      , DL.ppCurrent = DL.xmobarColor "white" "" . DL.wrap "[" "]"
+      , DL.ppTitle = const ""
+      }
 
 myConfig = def
   { terminal = "st"
@@ -61,6 +73,8 @@ myConfig = def
 
 myModMask = mod4Mask
 
+myFont = "xft:Terminus:style=Regular:size=12:antialias=false"
+
 myKeys =
   -- Window manager
   [ ("M-/", safeSpawn "rofi" ["-show", "run"])
@@ -73,6 +87,7 @@ myKeys =
   , ("M-S-n", shiftTo Next NonEmptyWS)
   , ("M-S-p", shiftTo Prev NonEmptyWS)
   , ("M-s", scratchpadSpawnActionCustom "st -n scratchpad")
+  , ("M-S-q", confirmPrompt promptConfig "exit" $ io (exitWith ExitSuccess))
   -- Brightness
   , ("<XF86MonBrightnessDown>", safeSpawn "light" ["-U", "5"])
   , ("<XF86MonBrightnessUp>", safeSpawn "light" ["-A", "5"])
@@ -81,6 +96,13 @@ myKeys =
   , ("<XF86AudioRaiseVolume>", safeSpawn "amixer" ["-q", "sset", "Master", "10%+"])
   , ("<XF86AudioMute>", safeSpawn "amixer" ["-q", "sset", "Master", "toggle"])
   ]
+    where promptConfig = def
+            { Prompt.fgColor = "red"
+            , Prompt.bgColor = "black"
+            , Prompt.position = Prompt.Top
+            , Prompt.font = myFont
+            , Prompt.promptBorderWidth = 0
+            }
 
 myMouse =
   [ ((myModMask, button3), (\w -> focus w >> mouseResizeEdgeWindow (1/2) w))
@@ -93,15 +115,16 @@ myStartupHook = do
 
 myLayoutHook =
   let tall = renamed [Replace "Tall"] $ ResizableTall 1 (1/20) (1/2) []
-  in toggleLayouts Full tall
-  & spacingRaw True (Border 5 5 5 5) True (Border 5 5 5 5) True
+      tabs = renamed [Replace "Tabs"] $ tabbed shrinkText (Themes.theme myTheme)
+  in toggleLayouts Full (tall ||| tabs)
+  & renamed [CutWordsLeft 1] . spacingRaw True (Border 5 5 5 5) True (Border 5 5 5 5) True
   & smartBorders
   & avoidStruts
-  & renamed [CutWordsLeft 1]
 
 myManageHook = manageHook def
   <+> manageDocks
   <+> scratchpadManageHook (RationalRect (1/4) (1/4) (1/2) (1/2))
+  <+> insertPosition Below Newer
 
 myHandleEventHook = handleEventHook def
   <+> fullscreenEventHook
@@ -109,5 +132,22 @@ myHandleEventHook = handleEventHook def
 
 myLogHook = logHook def
   <+> updatePointer (0.5, 0.5) (0, 0)
+
+myTheme :: Themes.ThemeInfo
+myTheme = Themes.xmonadTheme
+  { Themes.theme = def
+    { Deco.activeColor = "black"
+    , Deco.inactiveColor = "#333333"
+    , Deco.urgentColor = "#333333"
+    , Deco.activeBorderColor = "black"
+    , Deco.inactiveBorderColor = "#333333"
+    , Deco.urgentBorderColor = "#333333"
+    , Deco.activeTextColor = "white"
+    , Deco.inactiveTextColor = "#999999"
+    , Deco.urgentTextColor = "red"
+    , Deco.fontName = myFont
+    , Deco.decoHeight = 20
+    }
+  }
 
 {-# ANN module "HLint: Redundant return" #-}
