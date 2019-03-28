23b7de61e4532a8bbef582663d0a66e0c87dbfd3
Evan Relf <evan@evanrelf.com>
2019-03-27 23:40:45 -0700

    2019-03-27 #4 (aconfmgr for Arch)

---
+++ aconfmgr/.config/aconfmgr/20-config.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sed -i 's/^#\(en_US.UTF-8\)/\1/g' "$(GetPackageOriginalFile glibc /etc/locale.gen)"
+echo 'LANG=en_US.UTF-8' > "$(CreateFile /etc/locale.conf)"
+sed -i 's/^#\(Color\)/\1/g' "$(GetPackageOriginalFile pacman /etc/pacman.conf)"
+CreateLink /etc/localtime /usr/share/zoneinfo/America/Los_Angeles
---
+++ aconfmgr/.config/aconfmgr/30-packages.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+# Groups
+AddPackageGroup base
+AddPackageGroup base-devel
+
+# Packages
+AddPackage acpi # Client for battery, power, and thermal readings
+AddPackage alsa-utils # An alternative implementation of Linux sound support
+AddPackage alsa-utils # An alternative implementation of Linux sound support
+AddPackage autocutsel # synchronizes the two copy/paste buffers mainly used by X applications
+AddPackage autojump # A faster way to navigate your filesystem from the command line
+AddPackage btrfs-progs # Btrfs filesystem utilities
+AddPackage diff-so-fancy # Good-looking diffs with diff-highlight and more
+AddPackage docker # Pack, ship and run any application as a lightweight container
+AddPackage docker # Pack, ship and run any application as a lightweight container
+AddPackage dunst # Customizable and lightweight notification-daemon
+AddPackage exa # ls replacement
+AddPackage fd # Simple, fast and user-friendly alternative to find
+AddPackage feh # Fast and light imlib2-based image viewer
+AddPackage firefox # Standalone web browser from mozilla.org
+AddPackage fish # Smart and user friendly shell intended mostly for interactive use
+AddPackage fzf # Command-line fuzzy finder
+AddPackage intel-ucode # Microcode update files for Intel CPUs
+AddPackage kitty # A modern, hackable, featureful, OpenGL based terminal emulator
+AddPackage light # Program to easily change brightness on backlight-controllers.
+AddPackage light # Program to easily change brightness on backlight-controllers.
+AddPackage lvm2 # Logical Volume Manager 2 utilities
+AddPackage mosh # Mobile shell, surviving disconnects with local echo and line editing
+AddPackage mupdf # Lightweight PDF and XPS viewer
+AddPackage neovim # Fork of Vim aiming to improve user experience, plugins, and GUIs
+AddPackage networkmanager # Network connection manager and user applications
+AddPackage nodejs # Evented I/O for V8 javascript
+AddPackage nodejs-material-design-icons # Material Design icons by Google
+AddPackage noto-fonts-emoji # Google Noto emoji fonts
+AddPackage npm # A package manager for javascript
+AddPackage pandoc # Conversion between markup formats
+AddPackage physlock # Lightweight Linux console locking tool
+AddPackage powertop # A tool to diagnose issues with power consumption and power management
+AddPackage pulseaudio # A featureful, general-purpose sound server
+AddPackage pulseaudio # A featureful, general-purpose sound server
+AddPackage pulseaudio-alsa # ALSA Configuration for PulseAudio
+AddPackage pulseaudio-alsa # ALSA Configuration for PulseAudio
+AddPackage redshift # Adjusts the color temperature of your screen according to your surroundings.
+AddPackage ripgrep # A search tool that combines the usability of ag with the raw speed of grep
+AddPackage rofi # A window switcher, application launcher and dmenu replacement
+AddPackage rustup # The Rust toolchain installer
+AddPackage shellcheck # Shell script analysis tool
+AddPackage stack # The Haskell Tool Stack
+AddPackage stow # Manage installation of multiple softwares in the same directory tree
+AddPackage tlp # Linux Advanced Power Management
+AddPackage unarchiver # unar and lsar
+AddPackage unclutter # A small program for hiding the mouse cursor
+AddPackage xautolock # An automatic X screen-locker/screen-saver
+AddPackage xclip # Command line interface to the X11 clipboard
+AddPackage xf86-video-intel # X.org Intel i810/i830/i915/945G/G965+ video drivers
+AddPackage xmonad # Lightweight X11 tiled window manager written in Haskell
+AddPackage xmonad-contrib # Add-ons for xmonad
+AddPackage xorg-server # Xorg X server
+AddPackage xorg-xinit # X.Org initialisation program
+AddPackage xorg-xinput # Small commandline tool to configure devices
+AddPackage xorg-xrandr # Primitive command line interface to RandR extension
+AddPackage xorg-xrdb # X server resource database utility
+AddPackage xorg-xset # User preference utility for X
+AddPackage xorg-xsetroot # Classic X utility to set your root window background to a given pattern or color
+AddPackage zathura # Minimalistic document viewer
+
+# AUR
+AddPackage --foreign aconfmgr-git # A configuration manager for Arch Linux
+AddPackage --foreign polybar # A fast and easy-to-use status bar
+AddPackage --foreign tealdeer # An implementation of tldr in Rust
+AddPackage --foreign thermald # The Linux Thermal Daemon program from 01.org
+AddPackage --foreign yay # Yet another yogurt. Pacman wrapper and AUR helper written in go.
