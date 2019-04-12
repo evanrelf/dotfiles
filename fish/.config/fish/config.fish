f4c5bcdfade1aabaf001132e629a0ee3f8437c2d
Evan Relf <evan@evanrelf.com>
2019-04-12 09:36:12 -0700

    2019-04-12

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -30,13 +30,13 @@ if not functions -q fisher
     fisher
 end
 
-set -x FZF_LEGACY_KEYBINDINGS 0
-set -x FZF_DEFAULT_OPTS "--exact --height $FZF_TMUX_HEIGHT"
-set -x FZF_DEFAULT_COMMAND "fd --type file --follow --hidden --exclude '.git'"
-set -x FZF_FIND_FILE_COMMAND "$FZF_DEFAULT_COMMAND"
-set -x FZF_CD_COMMAND "fd --type directory --follow"
-set -x FZF_CD_WITH_HIDDEN_COMMAND "$FZF_CD_COMMAND --hidden --exclude '.git'"
-set -x FZF_OPEN_COMMAND "$FZF_FIND_FILE_COMMAND"
+set -U FZF_LEGACY_KEYBINDINGS 0
+set -U FZF_DEFAULT_OPTS "--exact --height $FZF_TMUX_HEIGHT"
+set -U FZF_DEFAULT_COMMAND "fd --type file --follow --hidden --exclude '.git'"
+set -U FZF_FIND_FILE_COMMAND "$FZF_DEFAULT_COMMAND"
+set -U FZF_CD_COMMAND "fd --type directory --follow"
+set -U FZF_CD_WITH_HIDDEN_COMMAND "$FZF_CD_COMMAND --hidden --exclude '.git'"
+set -U FZF_OPEN_COMMAND "$FZF_FIND_FILE_COMMAND"
 
 
 # VARIABLES {{{1
@@ -66,6 +66,57 @@ for i in $paths
         set -x PATH "$i" $PATH
     end
 end
+
+
+# KEYMAPPINGS {{{1
+
+function hybrid_bindings
+    # fish_default_key_bindings -M default
+    fish_default_key_bindings -M insert
+    # fish_default_key_bindings -M visual
+    fish_vi_key_bindings --no-erase
+end
+
+set -U fish_cursor_default block
+set -U fish_cursor_unknown block
+set -U fish_cursor_insert line
+set -U fish_cursor_visual block
+
+# Default
+fish_default_key_bindings
+# Hybrid
+# set -U fish_key_bindings hybrid_bindings
+# Vim
+# fish_vi_key_bindings --no-erase
+
+# Uncomment this to use a line cursor shape with the default keybindings
+function fish_default_cursor_handle --on-variable fish_bind_mode --on-event fish_postexec --on-event fish_focus_in
+    __fish_cursor_xterm line
+end
+
+function fish_default_cursor_handle_preexec --on-event fish_preexec
+    __fish_cursor_xterm line
+end
+
+# Uncomment this to use a dynamic cursor shape with the hybrid or vim keybindings
+# if test $TERM = "xterm-kitty"
+#     function fish_vi_cursor_handle --on-variable fish_bind_mode --on-event fish_postexec --on-event fish_focus_in
+#         set -l varname fish_cursor_$fish_bind_mode
+#         if not set -q $varname
+#             set varname fish_cursor_unknown
+#         end
+#         __fish_cursor_xterm $$varname
+#     end
+#     function fish_vi_cursor_handle_preexec --on-event fish_preexec
+#         set -l varname fish_cursor_default
+#         if not set -q $varname
+#             set varname fish_cursor_unknown
+#         end
+#         __fish_cursor_xterm $$varname
+#     end
+# else
+#     fish_vi_cursor
+# end
 
 
 # COMMANDS {{{1
