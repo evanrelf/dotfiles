36458e337e24df2559baf2c3c99711b095076124
Evan Relf <evan@evanrelf.com>
2019-04-29 11:56:16 -0700

    2019-04-29 #2

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -31,8 +31,8 @@ if not functions -q fisher
 end
 
 set -U FZF_LEGACY_KEYBINDINGS 0
-set -U FZF_DEFAULT_OPTS "--exact --height $FZF_TMUX_HEIGHT"
-set -U FZF_DEFAULT_COMMAND "fd --type file --follow --hidden --exclude '.git'"
+set -U FZF_DEFAULT_OPTS "--exact --height $FZF_TMUX_HEIGHT --reverse --ansi"
+set -U FZF_DEFAULT_COMMAND "fd --type file --follow --hidden --exclude '.git' --color=always"
 set -U FZF_FIND_FILE_COMMAND "$FZF_DEFAULT_COMMAND"
 set -U FZF_CD_COMMAND "fd --type directory --follow"
 set -U FZF_CD_WITH_HIDDEN_COMMAND "$FZF_CD_COMMAND --hidden --exclude '.git'"
