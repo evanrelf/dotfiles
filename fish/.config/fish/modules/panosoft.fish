bbc4def4764ce1f83be891268de8fac8121972b4
Evan Relf <erelf@panosoft.com>
2019-06-21 16:35:18 -0700

    2019-06-21

--- fish/.config/fish/modules/panosoft.fish
+++ fish/.config/fish/modules/panosoft.fish
@@ -1,6 +1,6 @@
-alias gauntlet "~/Code/evan.relf/scripts/gauntlet/gauntlet"
-alias qa "~/Code/evan.relf/scripts/qa/qa"
-alias vpn "~/Code/evan.relf/scripts/vpn/vpn"
+alias gauntlet "~/Code/work/evan.relf/scripts/gauntlet/gauntlet"
+alias qa "~/Code/work/evan.relf/scripts/qa/qa"
+alias vpn "~/Code/work/evan.relf/scripts/vpn/vpn"
 abbr --add nr "npm run"
 abbr --add sql "psql -d vetpro -U postgres -h localhost -p 5432"
 
