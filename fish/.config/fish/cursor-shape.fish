c4529a250865ec3d6e1bad3603da8ebe50091b83
Evan Relf <evan@evanrelf.com>
2020-06-29 21:45:13 -0700

    Start from scratch

--- fish/.config/fish/cursor-shape.fish
+++
@@ -1,7 +0,0 @@
-function fish_default_cursor_handle --on-variable fish_bind_mode --on-event fish_postexec --on-event fish_focus_in
-    __fish_cursor_xterm line
-end
-
-function fish_default_cursor_handle_preexec --on-event fish_preexec
-    __fish_cursor_xterm line
-end
--- xmonad/.config/xmonad/xmonad.hs
+++
@@ -1,1 +0,0 @@
-my-xmonad/xmonad.hs
