8c395335d9929f6b46267e4665a372b8d40e04cc
Evan Relf <evan@evanrelf.com>
2020-07-24 20:12:36 -0700

    Simplify and speed up XMonad recompilation

--- xmonad/.xmonad/shell.nix
+++ xmonad/.xmonad/shell.nix
@@ -1,1 +1,14 @@
-(import ./.).env
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  ghc =
+    pkgs.haskellPackages.ghcWithPackages (h: with h; [
+      base
+      xmonad
+      xmonad-contrib
+    ]);
+
+in
+  pkgs.mkShell {
+    buildInputs = [ ghc ];
+  }
--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,45 +1,44 @@
+{-# OPTIONS_GHC -Wno-missing-signatures #-}
+
 module Main (main) where
 
 import XMonad (XConfig (..), def, mod4Mask, xmonad)
 import qualified XMonad.Actions.CycleWS as CycleWS
 import qualified XMonad.Util.EZConfig as EZConfig
 import qualified XMonad.Util.Run as Run
+import XMonad.Layout.Decoration (Theme (..))
+import XMonad.Layout.NoFrillsDecoration (noFrillsDeco, shrinkText)
+import Data.Function ((&))
 
 
-main :: IO ()
-main = xmonad
-  . myKeys
-  . myRemoveKeys
-  . myConfig
-  $ def
-
-
-myConfig :: XConfig l -> XConfig l
-myConfig xconfig = xconfig
+main = xmonad $ def
   { terminal = "kitty"
   , modMask = mod4Mask
   , focusFollowsMouse = False
   , clickJustFocuses = True
-  , borderWidth = 4
-  , normalBorderColor = "#777777"
-  , focusedBorderColor = "#BB77FF"
+  , borderWidth = 3
+  , normalBorderColor = myInactiveColor
+  , focusedBorderColor = myActiveColor
   , workspaces = fmap show [1 .. 10 :: Int]
   -- , startupHook = undefined
   -- , logHook = undefined
-  -- , layoutHook = undefined
+  , layoutHook = myLayoutHook
   -- , manageHook = undefined
   -- , handleEventHook = undefined
   }
+  & myRemoveKeys
+  & myKeys
 
 
-myRemoveKeys :: XConfig l -> XConfig l
+myLayoutHook = noFrillsDeco shrinkText myTheme (layoutHook def)
+
+
 myRemoveKeys xconfig = EZConfig.removeKeysP xconfig
   -- Focus previous window
   [ "M-S-<Tab>"
   ]
 
 
-myKeys :: XConfig l -> XConfig l
 myKeys xconfig = EZConfig.additionalKeysP xconfig
   -- Launching
   [ ("M-d", Run.safeSpawnProg "dmenu_run")
@@ -53,3 +52,19 @@ myKeys xconfig = EZConfig.additionalKeysP xconfig
   , ("<XF86AudioRaiseVolume>", Run.safeSpawn "amixer" ["-q", "sset", "Master", "10%+"])
   , ("<XF86AudioMute>", Run.safeSpawn "amixer" ["-q", "sset", "Master", "toggle"])
   ]
+
+
+myActiveColor = "#1A11A1"
+myInactiveColor = "#555555"
+
+
+myTheme = def
+  { activeColor = myActiveColor
+  , inactiveColor = myInactiveColor
+  , activeTextColor = myActiveColor
+  , inactiveTextColor = myInactiveColor
+  , activeBorderWidth = 0
+  , inactiveBorderWidth = 0
+  , urgentBorderWidth = 0
+  , decoHeight = 10
+  }
