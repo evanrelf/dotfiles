93e15387250bb5aa313143233bd335391138210e
Evan Relf <evan@evanrelf.com>
2020-07-27 18:41:54 -0700

    Purge rarely used packages from `env.nix`

--- nix/.config/nix/env.nix
+++ nix/.config/nix/env.nix
@@ -12,78 +12,35 @@ in
   pkgs.buildEnv {
     name = "env";
     paths = with pkgs; [
-      (aspellWithDicts (d: with d; [ en en-computers en-science ]))
-      (haskell.lib.justStaticExecutables haskellPackages.fast-tags)
-      (haskell.lib.justStaticExecutables haskellPackages.wai-app-static)
-      borgbackup
       cabal-install
-      cabal-plan
-      cabal2nix
-      cachix
-      cmatrix
-      coin
       comma
-      cowsay
       declarative-channels
-      deno
-      dhall
-      dhall-json
-      emacs
       exa
       fd
-      ffmpeg-full
       fish
-      fortune
       fzf
-      gay
       gcoreutils
       ghcid
-      # ghcide
       git
       gitAndTools.delta
-      gitAndTools.hub
-      gti
-      hlint
-      htop
-      httpie
-      iosevka-pro
       jq
       kakoune
-      lolcat
-      lorri
-      moreutils
       neovim
-      nix-diff
-      nix-index
       nix-prefetch-git
-      nix-tree
-      # nodejs
-      nix
-      # nixUnstable
-      ormolu
-      pandoc
-      pipes
-      python3
-      racket-minimal
       ripgrep
       rust-analyzer
       rustup
       sd
       shellcheck
-      sl
       tealdeer
       tmux
-      toilet
-      tokei
-      tree
-      universal-ctags
-      youtube-dl
       zoxide
-      zsh
     ] ++ (pkgs.lib.optionals pkgs.stdenv.isLinux [
       acpi
       dmenu
+      emacs
       firefox
+      iosevka-pro
       kitty
       xclip
     ]);
