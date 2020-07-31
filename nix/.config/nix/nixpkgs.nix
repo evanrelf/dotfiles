c5d63e2e6b31154f150a3f643f3c51593a7650ee
Evan Relf <evan@awakesecurity.com>
2020-07-30 19:01:55 -0700

    Bump pinned Nixpkgs

--- nix/.config/nix/nixpkgs.nix
+++ nix/.config/nix/nixpkgs.nix
@@ -1,13 +1,13 @@
 args:
 
 let
-  rev = "c27e54de99df793756a5314f8fd5dd3e49d31927";
-
+  # master on 2020-07-30
+  rev = "b1ac18b7acc54ba6086b51875ea245113821c050";
   nixpkgs =
     builtins.fetchTarball {
       name = "nixpkgs-unstable";
       url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-      sha256 = "0289m555hpmml5g6idicg1bckphww22p87s8qca2k046zkz0ykx0";
+      sha256 = "0if3i8w5f2p8fm8bfnj0hlx7yhz3b5cqhq5gqzs1i542bzy5d6ab";
     };
 
 in
