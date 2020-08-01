1c8ed8dede0956836c89df195d21e0e12b11725c
Evan Relf <evan@evanrelf.com>
2020-07-31 23:52:59 -0700

    nix: Clean up overlay
    Removed outdated or no longer used packages.

--- nix/.config/nix/overlay.nix
+++ nix/.config/nix/overlay.nix
@@ -18,17 +18,6 @@ self: super:
           (old.executableHaskellDepends or []) ++ (with self.haskellPackages; [ tar zlib ]);
       }));
 
-  coin =
-    let
-      coinSound = super.fetchurl {
-        url = "https://themushroomkingdom.net/sounds/wav/smw/smw_coin.wav";
-        sha256 = "18c7dfhkaz9ybp3m52n1is9nmmkq18b1i82g6vgzy7cbr2y07h93";
-      };
-    in
-      super.writeShellScriptBin "coin" ''
-         ${self.sox}/bin/play --no-show-progress ${coinSound}
-      '';
-
   comma =
     import (super.fetchFromGitHub {
       owner = "shopify";
@@ -39,16 +28,6 @@ self: super:
 
   # emacs =
   #   super.emacsWithPackages [ super.emacsPackages.vterm ];
-
-  gay =
-    super.python3Packages.buildPythonPackage rec {
-      pname = "gay";
-      version = "1.2.1";
-      src = super.python3Packages.fetchPypi {
-        inherit pname version;
-        sha256 = "0kjraippbkm1fgmky7dfww4an2yj53y4fcsganps1dhgn1mfs2ks";
-      };
-    };
 
   gcoreutils =
     super.coreutils.override {
@@ -63,27 +42,6 @@ self: super:
       rev = "67493b873e1a5e5d53837401ab6b128b20e8a989";
       sha256 = "1zq5g7ka99vcyqbg5l1bx0rliq3ihig37nzczk0wdwidjyxjghf9";
     }) {}).ghcide-ghc865;
-
-  # gormolu =
-  #   let
-  #     source =
-  #       super.fetchFromGitHub {
-  #         owner = "tweag";
-  #         repo = "ormolu";
-  #         rev = "b1fc69265f8688490b16de83bb235d29aa25bcb0";
-  #         sha256 = "1yk3yh0rg0gcnhyd52jwqgah09yczbk21gb80xhrighacmvdsnwn";
-  #       };
-  #     haskellPackage =
-  #       (import source { pkgs = self; }).ormolu.override {
-  #         ghc-lib-parser = self.haskellPackages.ghc-lib-parser_8_10_1_20200523;
-  #       };
-  #     executable =
-  #       super.haskell.lib.justStaticExecutables haskellPackage;
-  #   in
-  #     super.runCommandLocal "gormolu" {} ''
-  #       mkdir -p $out/bin
-  #       ln -s ${executable}/bin/ormolu $out/bin/gormolu
-  #     '';
 
   iosevka-pro =
     # To install on macOS:
@@ -120,38 +78,6 @@ self: super:
         ${old.preConfigure}
         export version="${version}"
       '';
-    });
-
-  lf =
-    super.buildGoModule {
-      pname = "lf";
-      version = "r14";
-      src = super.fetchFromGitHub {
-        owner = "gokcehan";
-        repo = "lf";
-        rev = "r14";
-        sha256 = "0kl9yrgph1i0jbxhlg3k0411436w80xw1s8dzd7v7h2raygkb4is";
-      };
-      vendorSha256 = "1zb2z3c2w4gnq9cjczg1y7r7jg4mlrm2hsb12dqd9w8mh44rvr37";
-    };
-
-  lorri =
-    import (super.fetchFromGitHub {
-      owner = "target";
-      repo = "lorri";
-      rev = "1.1";
-      sha256 = "0wbkx8hmikngfp6fp0y65yla22f3k0jszq8a6pas80q0b33llwm5";
-    }) { pkgs = self; };
-
-  nixUnstable =
-    super.nixUnstable.overrideAttrs (old: {
-      name = "nix-2.4";
-      src = super.fetchFromGitHub {
-        owner = "NixOS";
-        repo = "nix";
-        rev = "3f01fa1c9c1b5ce0ffd701f8eeb1b16e57aa86d5";
-        sha256 = "1lgpi7xzalf4wpgg3yw72v579cmrplgjhpn1l648bxzwjjp3x7pn";
-      };
     });
 
   nix-tree =
