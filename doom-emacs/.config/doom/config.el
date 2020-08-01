4994951aed8939f5bf15e67f958afab67c809d4b
Evan Relf <evan@evanrelf.com>
2020-07-31 17:34:33 -0700

    doom: Use different font on Linux

--- doom-emacs/.config/doom/config.el
+++ doom-emacs/.config/doom/config.el
@@ -81,8 +81,11 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "PragmataPro Liga" :size 16)
-      doom-variable-pitch-font (font-spec :family "PragmataPro Liga" :size 16))
+(if (eq system-type 'darwin)
+  (setq doom-font (font-spec :family "PragmataPro Liga" :size 16)
+        doom-variable-pitch-font (font-spec :family "PragmataPro Liga" :size 16))
+  (setq doom-font (font-spec :family "Iosevka Pro" :size 27)
+        doom-variable-pitch-font (font-spec :family "Iosevka Pro" :size 27)))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
