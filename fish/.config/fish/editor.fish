415b665aecbfd77420db8b1295444cc3600d7eb7
Evan Relf <evan@evanrelf.com>
2020-08-02 15:19:45 -0700

    Miscellaneous cleanup

--- fish/.config/fish/editor.fish
+++ fish/.config/fish/editor.fish
@@ -11,13 +11,13 @@ end
 # Better support for Emacs' vterm package
 function vterm_printf
     if test -n "$TMUX"
-	# tell tmux to pass the escape sequences through
-	# (Source: http://permalink.gmane.org/gmane.comp.terminal-emulators.tmux.user/1324)
-	printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
+        # tell tmux to pass the escape sequences through
+        # (Source: http://permalink.gmane.org/gmane.comp.terminal-emulators.tmux.user/1324)
+        printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
     else if string match -q -- "screen*" "$TERM"
-	# GNU screen (screen, screen-256color, screen-256color-bce)
-	printf "\eP\e]%s\007\e\\" "$argv"
+        # GNU screen (screen, screen-256color, screen-256color-bce)
+        printf "\eP\e]%s\007\e\\" "$argv"
     else
-	printf "\e]%s\e\\" "$argv"
+        printf "\e]%s\e\\" "$argv"
     end
 end
