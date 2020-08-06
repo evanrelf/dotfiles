61f2d29e323c8a2fc16867a6b887c5398f4b66f4
Evan Relf <evan@evanrelf.com>
2020-08-06 09:46:07 -0700

    doom: Disable auto-complete with `company`
    It may be to blame for the lag I'm getting in `nix-mode`. I also don't
    value it all that much, and I don't have any LSP stuff set up so I'm not
    getting very interesting completions anyways.

--- doom/.config/doom/init.el
+++ doom/.config/doom/init.el
@@ -19,7 +19,7 @@
        ;;japanese
 
        :completion
-       (company +tng)    ; the ultimate code completion backend
+       ;;(company +tng)    ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        (ivy +prescient)  ; a search engine for love and life
