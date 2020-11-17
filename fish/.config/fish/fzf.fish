bd103f01354ae1c40006f151344e7ff5cc03b8c8
Evan Relf <evan@evanrelf.com>
2020-11-17 14:43:33 -0800

    fish: Make `fzf.fish` `cd` into hidden directories

--- fish/.config/fish/fzf.fish
+++ fish/.config/fish/fzf.fish
@@ -1,9 +1,9 @@
 if _exists fzf
     set --export FZF_LEGACY_KEYBINDINGS 0
-    set --export FZF_DEFAULT_OPTS "--color=light --exact --height $FZF_TMUX_HEIGHT --reverse --ansi"
+    set --export FZF_DEFAULT_OPTS "--color=light --exact --height $FZF_TMUX_HEIGHT --reverse --ansi --no-multi"
     set --export FZF_DEFAULT_COMMAND "fd --type file --follow --hidden --exclude '.git' --color=always"
     set --export FZF_FIND_FILE_COMMAND "$FZF_DEFAULT_COMMAND"
-    set --export FZF_CD_COMMAND "fd --type directory --follow"
-    set --export FZF_CD_WITH_HIDDEN_COMMAND "$FZF_CD_COMMAND --hidden --exclude '.git'"
+    set --export FZF_CD_COMMAND "fd --type directory --follow --hidden --exclude '.git'"
+    set --export FZF_CD_WITH_HIDDEN_COMMAND "$FZF_CD_COMMAND"
     set --export FZF_OPEN_COMMAND "$FZF_FIND_FILE_COMMAND"
 end
