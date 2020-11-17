dc95ef26124769f66b31472ddd944b4aad7c3122
Evan Relf <evan@awakesecurity.com>
2020-11-17 14:44:53 -0800

    nix: Bump pinned Nixpkgs

--- nix/.config/nix/nixpkgs.nix
+++ nix/.config/nix/nixpkgs.nix
@@ -1,13 +1,13 @@
 args:
 
 let
-  # master on 2020-09-19
-  rev = "d61c16cda48a581dfecfce69099773d9737d5b76";
+  # master on 2020-11-17
+  rev = "87aa3671e803f32617e899980c72dd7bc308cd1e";
   nixpkgs =
     builtins.fetchTarball {
       name = "nixpkgs-unstable";
       url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-      sha256 = "1y9yc74acxkcw8pxibqafmx8nz622xnd3i9b1yqsbxm4xqwkzl35";
+      sha256 = "1xy3ys1z8vh42vmj2i2y0j352p8q0j8hg2zliavdrjz6447q9136";
     };
 
 in
