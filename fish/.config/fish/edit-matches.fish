823cab31482e68373257a5c89f4b0982bae98e46
Evan Relf <evan@evanrelf.com>
2020-12-22 18:36:34 -0800

    fish: Completely revamp `edit-matches`
    - `edit-matches` is renamed to `rges`
    - New `rge` function to edit a single match, narrowed from a list of
    matches using `fzf`
    - Common functionality factored out into `__edit_file_position`

--- fish/.config/fish/edit-matches.fish
+++ fish/.config/fish/edit-matches.fish
@@ -1,13 +1,33 @@
-function edit-matches -d "Edit files containing a matching regular expression"
-    for file in (rg --files-with-matches $argv)
-        if echo "$EDITOR" | rg --quiet "kak"
-            set --local position (rg --line-number --column --max-count 1 $argv -- $file | cut -d ':' -f 1-2)
-            eval "$EDITOR $file +$position" || break
-        else if echo "$EDITOR" | rg --quiet "(vim|nvim)"
-            set --local line (rg --line-number --max-count 1 $argv -- $file | cut -d ':' -f 1)
-            eval "$EDITOR $file +$line" || break
-        else
-            eval "$EDITOR $file" || break
-        end
+function __edit_file_position
+    if test -z "$argv"
+        return 1
+    end
+
+    _log "$argv"
+
+    set --local file (echo "$argv" | cut -d ':' -f 1)
+    set --local line (echo "$argv" | cut -d ':' -f 2)
+    set --local column (echo "$argv" | cut -d ':' -f 3)
+
+    switch "$EDITOR"
+        case "kak"
+            eval "$EDITOR $file +$line:$column"
+        case "vi" "vim" "nvim"
+            eval "$EDITOR $file '+normal $line''G$column|zz'"
+        case "*"
+            eval "$EDITOR $file"
+    end
+end
+
+function rge -d "Edit file containing a matching regular expression"
+    set --local rg_cmd "rg --line-number --column --with-filename --color always $argv"
+    set --local fzf_cmd "fzf --exact -0"
+    set --local match (eval "$rg_cmd | $fzf_cmd")
+    __edit_file_position "$match" && rge $argv
+end
+
+function rges -d "Edit files containing a matching regular expression"
+    for match in (rg --line-number --column --with-filename --max-count 1 $argv)
+        __edit_file_position "$match" || return 1
     end
 end
