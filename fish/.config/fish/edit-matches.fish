f71b3b417c373bb973870626d0a7b979defef54d
Evan Relf <evan@evanrelf.com>
2020-12-22 17:32:49 -0800

    fish: Make `edit-matches` open at exact positions

--- fish/.config/fish/edit-matches.fish
+++ fish/.config/fish/edit-matches.fish
@@ -1,5 +1,13 @@
 function edit-matches -d "Edit files containing a matching regular expression"
     for file in (rg --files-with-matches $argv)
-        eval "$EDITOR $file" || break
+        if echo "$EDITOR" | rg --quiet "kak"
+            set --local position (rg --line-number --column --max-count 1 $argv -- $file | cut -d ':' -f 1-2)
+            eval "$EDITOR $file +$position" || break
+        else if echo "$EDITOR" | rg --quiet "(vim|nvim)"
+            set --local line (rg --line-number --max-count 1 $argv -- $file | cut -d ':' -f 1)
+            eval "$EDITOR $file +$line" || break
+        else
+            eval "$EDITOR $file" || break
+        end
     end
 end
