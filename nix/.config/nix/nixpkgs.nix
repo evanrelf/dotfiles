0350bea30f02783a5ad35ff3b2e3ef157d158b9d
Evan Relf <evanrelf@auburn.local>
2020-12-30 14:20:51 -0800

    nix: Bump Nixpkgs pin

--- nix/.config/nix/nixpkgs.nix
+++ nix/.config/nix/nixpkgs.nix
@@ -1,14 +1,14 @@
 args:
 
 let
-  # master on 2020-11-17
-  rev = "87aa3671e803f32617e899980c72dd7bc308cd1e";
+  # master on 2020-12-30
+  rev = "9b78f36021b3530773c4a2136c26862d32aef3f6";
 
   nixpkgs =
     builtins.fetchTarball {
-      name = "nixpkgs-unstable";
+      name = "nixpkgs";
       url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-      sha256 = "1xy3ys1z8vh42vmj2i2y0j352p8q0j8hg2zliavdrjz6447q9136";
+      sha256 = "1b2apazjm9n4phh6gvq9dw6x6ziklvvqahz7wy5gmbmyaxk8yf1g";
     };
 
 in
