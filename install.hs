3ab324439b7dccfbb6cdad374cf41feb4bd5d00c
Evan Relf <evan@evanrelf.com>
2021-01-04 20:49:45 -0800

    install.hs: Simplify after stripping out "prepare"

--- install.hs
+++ install.hs
@@ -41,103 +41,30 @@ module Main (main) where
 
 import Data.String.Interpolate (i, iii)
 import Relude
-import System.FilePath ((</>))
 
 import qualified Data.List.NonEmpty as NonEmpty
 import qualified Options.Applicative as Options
 import qualified Relude.Extra.Bifunctor as Bifunctor
 import qualified System.Console.ANSI as Ansi
 import qualified UnliftIO.Directory as Directory
-import qualified UnliftIO.Environment as Environment
 import qualified UnliftIO.Process as Process
-
-
-data Env = Env
-  { options :: Options
-  , home :: FilePath
-  } deriving stock Show
-
-
---------------------------------------------------------------------------------
--- RUN
---------------------------------------------------------------------------------
 
 
 main :: IO ()
 main = do
-  rawOptions@Options{packages = rawPackages} <- getOptions
-
-  home <-
-    Environment.lookupEnv "HOME" `whenNothingM`
-      panic "Failed to get HOME environment variable"
+  Options{packages = rawPackages, dryRun} <- getOptions
 
   let stripSlashes = filter (/= '/')
 
   existentPackages <- discardNonexistent (fmap stripSlashes rawPackages)
 
   case nonEmpty existentPackages of
-    Nothing ->
-      panic "No packages specified"
+    Nothing -> do
+      putStrLnColored Ansi.Red "No packages specified"
+      exitFailure
 
-    Just packages -> do
-      let options = rawOptions{packages}
-      usingReaderT Env{options, home} do
-        mapM_ install packages
-
-
-discardNonexistent :: MonadIO m => NonEmpty FilePath -> m [FilePath]
-discardNonexistent packages = do
-  (existent, nonexistent) <- partitionM Directory.doesDirectoryExist packages
-
-  forM_ nonexistent \package ->
-    warn [i|[#{package}] Configuration doesn't exist|]
-
-  pure existent
-
-
---------------------------------------------------------------------------------
--- INSTALL
---------------------------------------------------------------------------------
-
-
-install :: (MonadIO m, MonadReader Env m) => FilePath -> m ()
-install package =
-  mapM_
-    ($ package)
-    [ hook "before"
-    , stow
-    , hook "after"
-    ]
-
-
-hook :: (MonadIO m, MonadReader Env m) => String -> FilePath -> m ()
-hook scriptPrefix package = do
-  let script = package </> scriptPrefix <> "-hook"
-
-  whenM (Directory.doesFileExist script) do
-    log [i|[#{package}] Running #{scriptPrefix} hook|]
-
-    sh [i|bash #{script}|]
-
-
-stow :: (MonadIO m, MonadReader Env m) => FilePath -> m ()
-stow package = do
-  log [i|[#{package}] Stowing configuration|]
-
-  assertExecutableExists "stow"
-
-  sh [iii|
-    stow
-      --stow
-      --target "$HOME"
-      --no-folding #{package}
-      --ignore "-hook"
-  |]
-
-
---------------------------------------------------------------------------------
--- PARSE CLI OPTIONS
---------------------------------------------------------------------------------
+    Just packages ->
+      mapM_ (usingReaderT dryRun . install) packages
 
 
 data Options = Options
@@ -169,14 +96,58 @@ parseOptions = do
   pure Options{packages, dryRun}
 
 
---------------------------------------------------------------------------------
--- HELPER FUNCTIONS
---------------------------------------------------------------------------------
+discardNonexistent :: MonadIO m => NonEmpty FilePath -> m [FilePath]
+discardNonexistent packages = do
+  (existent, nonexistent) <- partitionM Directory.doesDirectoryExist packages
+
+  forM_ nonexistent \package ->
+    putStrLnColored Ansi.Yellow [i|[#{package}] Configuration doesn't exist|]
+
+  pure existent
 
 
-sh :: (MonadIO m, MonadReader Env m) => String -> m ()
+partitionM :: Monad m => (a -> m Bool) -> NonEmpty a -> m ([a], [a])
+partitionM predicate xs = do
+  xs' <- mapM (\x -> (x, ) <$> predicate x) xs
+  pure $ Bifunctor.bimapBoth (fmap fst) (NonEmpty.partition snd xs')
+
+
+install :: (MonadIO m, MonadReader Bool m) => FilePath -> m ()
+install package =
+  mapM_
+    ($ package)
+    [ runHook "before"
+    , stow
+    , runHook "after"
+    ]
+
+
+runHook :: (MonadIO m, MonadReader Bool m) => String -> FilePath -> m ()
+runHook hookName package = do
+  let script = [i|package/#{hookName}-hook|]
+
+  whenM (Directory.doesFileExist script) do
+    log [i|[#{package}] Running #{hookName} hook|]
+
+    sh [i|./#{script}|]
+
+
+stow :: (MonadIO m, MonadReader Bool m) => FilePath -> m ()
+stow package = do
+  log [i|[#{package}] Stowing configuration|]
+
+  sh [iii|
+    stow
+      --stow
+      --target "$HOME"
+      --no-folding #{package}
+      --ignore "-hook"
+  |]
+
+
+sh :: (MonadIO m, MonadReader Bool m) => String -> m ()
 sh command = do
-  Env{options = Options{dryRun}} <- ask
+  dryRun <- ask
 
   if dryRun then
     log [i|dry-run> #{command}|]
@@ -186,10 +157,8 @@ sh command = do
     Process.callCommand command
 
 
-assertExecutableExists :: MonadIO m => FilePath -> m ()
-assertExecutableExists executable = do
-  Directory.findExecutable executable `whenNothingM_`
-    panic [i|Missing executable: #{executable}|]
+log :: MonadIO m => String -> m ()
+log = putStrLnColored Ansi.Magenta
 
 
 putStrLnColored :: MonadIO m => Ansi.Color -> String -> m ()
@@ -197,23 +166,3 @@ putStrLnColored color message = do
   let colored = Ansi.setSGRCode [Ansi.SetColor Ansi.Foreground Ansi.Dull color]
   let reset = Ansi.setSGRCode [Ansi.Reset]
   putStrLn (colored <> message <> reset)
-
-
-log :: MonadIO m => String -> m ()
-log = putStrLnColored Ansi.Magenta
-
-
-warn :: MonadIO m => String -> m ()
-warn = putStrLnColored Ansi.Yellow
-
-
-panic :: MonadIO m => String -> m a
-panic message = do
-  putStrLnColored Ansi.Red message
-  exitFailure
-
-
-partitionM :: Monad m => (a -> m Bool) -> NonEmpty a -> m ([a], [a])
-partitionM predicate xs = do
-  xs' <- mapM (\x -> (x, ) <$> predicate x) xs
-  pure $ Bifunctor.bimapBoth (fmap fst) (NonEmpty.partition snd xs')
