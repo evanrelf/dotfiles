31e06c1216ef5a77d5c720f085dfeb0132e2b512
Evan Relf <evan@awakesecurity.com>
2021-01-06 12:56:06 -0800

    nix: Add `bash`

--- nix/.config/nix/env.nix
+++ nix/.config/nix/env.nix
@@ -32,6 +32,7 @@ in
     name = "env";
     paths = with pkgs; [
       (aspellWithDicts (d: with d; [ en en-computers en-science ]))
+      bash
       cabal-install
       comma
       coreutils-gprefix
