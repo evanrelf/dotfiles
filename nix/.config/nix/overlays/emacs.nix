8a385a7b4f642a8521d088f0b641c4cbbed02ecf
Evan Relf <evan@awakesecurity.com>
2021-01-06 12:55:24 -0800

    nix: Reformat `emacs` overlay

--- nix/.config/nix/overlays/emacs.nix
+++ nix/.config/nix/overlays/emacs.nix
@@ -1,8 +1,11 @@
 let
   rev = "43c916bc555d9531142e1b5e912b4c7639dde916";
+  sha256 = "0yqqkmy006hjm4ji7q0q99d1z413pxk9mb74915rmhnl8h43ak1l";
+
+  emacsOverlay = builtins.fetchTarball {
+    url = "https://github.com/nix-community/emacs-overlay/archive/${rev}.tar.gz";
+    inherit sha256;
+  };
 
 in
-  import (builtins.fetchTarball {
-    url = "https://github.com/nix-community/emacs-overlay/archive/${rev}.tar.gz";
-    sha256 = "0yqqkmy006hjm4ji7q0q99d1z413pxk9mb74915rmhnl8h43ak1l";
-  })
+  import emacsOverlay
