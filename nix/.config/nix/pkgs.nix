60bf72807d72b2cc8565bf252e93c19e8038030c
Evan Relf <evan@evanrelf.com>
2021-03-24 13:50:44 -0700

    nix: Rename overlay `haskell{,-packages}`

--- nix/.config/nix/pkgs.nix
+++ nix/.config/nix/pkgs.nix
@@ -3,7 +3,7 @@ args:
 import ./nixpkgs.nix ({
   overlays = [
     (import ./overlays/top-level.nix)
-    (import ./overlays/haskell.nix)
+    (import ./overlays/haskell-packages.nix)
     (import ./overlays/kakoune-plugins.nix)
   ];
 } // args)
