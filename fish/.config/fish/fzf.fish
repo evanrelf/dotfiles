89833b47b9a41d2f2aee45a906f05b581206b9c3
Evan Relf <evan@evanrelf.com>
2021-04-18 23:24:52 -0700

    fish: Remove conflicting `fzf` `--color` option

--- fish/.config/fish/fzf.fish
+++ fish/.config/fish/fzf.fish
@@ -1,7 +1,7 @@
 if _exists fzf
     set --export FZF_LEGACY_KEYBINDINGS 0
     set --export FZF_DEFAULT_OPTS "--color=bw --exact --height $FZF_TMUX_HEIGHT --reverse --ansi --no-multi"
-    set --export FZF_DEFAULT_COMMAND "fd --type file --follow --hidden --exclude '.git' --color=always"
+    set --export FZF_DEFAULT_COMMAND "fd --type file --follow --hidden --exclude '.git'"
     set --export FZF_FIND_FILE_COMMAND "$FZF_DEFAULT_COMMAND"
     set --export FZF_CD_COMMAND "fd --type directory --follow --hidden --exclude '.git'"
     set --export FZF_CD_WITH_HIDDEN_COMMAND "$FZF_CD_COMMAND"
