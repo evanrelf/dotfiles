c0a86ecf22a6b786aac0cae42b1d708302ad24e4
Evan Relf <evan@evanrelf.com>
2021-05-30 12:29:06 -0700

    fish+starship: Handle new version of `zoxide`

--- fish/.config/fish/zoxide.fish
+++ fish/.config/fish/zoxide.fish
@@ -3,19 +3,5 @@ if _exists zoxide
     if test -f "$HOME/.zo"
         rm "$HOME/.zo"
     end
-    # Vendored from the official repo because I don't like the plugin is
-    # configured by default
-    function zoxide-add --on-event fish_prompt
-        zoxide add
-    end
-    function z
-        if test (count "$argv") -gt 0
-            set --local _Z_RESULT (zoxide query "$argv")
-            if test -d "$_Z_RESULT"
-              cd "$_Z_RESULT"
-            else
-              echo "$_Z_RESULT"
-            end
-        end
-    end
+    zoxide init fish | source
 end
