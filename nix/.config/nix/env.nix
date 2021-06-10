471ffa6589055c186192247fc7a6b4fd798a3a1f
Evan Relf <evan@awakesecurity.com>
2021-06-10 14:52:14 -0700

    nix: Specify packages per machine

--- nix/.config/nix/env.nix
+++ nix/.config/nix/env.nix
@@ -1,6 +1,9 @@
 { hostname ? throw "Please specify your machine's hostname (e.g. `--argstr hostname $(hostname -s)`)"
 }:
 
+# To install fonts on macOS:
+# $ open ~/.nix-profile/share/fonts/truetype/**/*.{ttf,ttc}
+
 let
   pkgs = import ./pkgs.nix { };
 
@@ -10,110 +13,109 @@ let
       ln -s ${pkgs.path} $out/channels/nixpkgs
     '';
 
-  traceIf = bool: message:
-    if bool then builtins.trace message true else false;
+  commonPackages = with pkgs; [
+    (aspellWithDicts (d: with d; [ en en-computers en-science ]))
+    as-tree
+    bashInteractive
+    cabal-install
+    coreutils-gprefix
+    declarative-channels
+    delta
+    dhall
+    direnv
+    # emacsCustom
+    exa
+    fd
+    findutils-gprefix
+    fish
+    fzf
+    ghcid
+    gitAndTools.delta
+    gitAndTools.gh
+    gitAndTools.gitFull
+    gnugrep-gprefix
+    haskellPackages.cabal-plan
+    haskellPackages.fourmolu
+    helix
+    htop
+    httpie
+    hyperfine
+    iosevka-bin
+    jq
+    kakoune
+    lorri
+    magic-wormhole
+    moreutils
+    neovim
+    nerdfonts
+    nix-diff
+    nix-index
+    nix-prefetch-git
+    nix-top
+    nix-tree
+    nixpkgs-fmt
+    ormoloog
+    ormolu
+    pandoc
+    parinfer-rust
+    perlPackages.GitAutofixup
+    ripgrep
+    rlwrap
+    sd
+    shellcheck
+    starship
+    stylish-haskell
+    tealdeer
+    tmux
+    tmux-thumbs
+    tokei
+    tree
+    watchexec
+    yj
+    zoxide
+  ];
 
-  isLinux =
-    traceIf
-      pkgs.stdenv.isLinux
-      "Installing extra packages for Linux machine";
+  personalPackages = with pkgs; [
+    borgbackup
+    ffmpeg
+    go
+    mercurialFull
+    rclone
+    youtube-dl
+  ];
 
-  isPersonalMachine =
-    traceIf
-      (builtins.elem hostname [ "auburn" "sienna" ])
-      "Installing extra packages for personal machine '${hostname}'";
+  linuxPackages = with pkgs; [
+    acpi
+    autocutsel
+    bemenu
+    dmenu # keep for `dmenu_path | bemenu`
+    firefox
+    kitty
+    noto-fonts
+    redshift
+    spotify
+    xbanish
+    xclip
+  ];
 
-  isWorkMachine =
-    traceIf
-      (builtins.elem hostname [ "indigo" ])
-      "Installing extra packages for work machine '${hostname}'";
-
-  # To install fonts on macOS:
-  # $ open ~/.nix-profile/share/fonts/truetype/**/*.{ttf,ttc}
 in
 pkgs.buildEnv {
   name = "env";
-  paths = with pkgs;
-    [
-      (aspellWithDicts (d: with d; [ en en-computers en-science ]))
-      as-tree
-      bashInteractive
-      cabal-install
-      coreutils-gprefix
-      declarative-channels
-      dhall
-      direnv
-      # emacsCustom
-      exa
-      fd
-      findutils-gprefix
-      fish
-      fzf
-      ghcid
-      gitAndTools.delta
-      gitAndTools.gh
-      gitAndTools.gitFull
-      gnugrep-gprefix
-      haskellPackages.cabal-plan
-      haskellPackages.fourmolu
-      helix
-      htop
-      httpie
-      hyperfine
-      iosevka-bin
-      jq
-      kakoune
-      lorri
-      magic-wormhole
-      moreutils
-      neovim
-      nerdfonts
-      nix-diff
-      nix-index
-      nixpkgs-fmt
-      nix-prefetch-git
-      nix-top
-      nix-tree
-      ormoloog
-      ormolu
-      pandoc
-      parinfer-rust
-      perlPackages.GitAutofixup
-      ripgrep
-      rlwrap
-      sd
-      shellcheck
-      starship
-      stylish-haskell
-      tealdeer
-      tmux
-      tmux-thumbs
-      tokei
-      tree
-      watchexec
-      yj
-      zoxide
-    ] ++ (pkgs.lib.optionals isLinux [
-      acpi
-      autocutsel
-      bemenu
-      dmenu # keep for `dmenu_path | bemenu`
-      firefox
-      kitty
-      noto-fonts
-      redshift
-      spotify
-      xbanish
-      xclip
-    ]) ++ (pkgs.lib.optionals isPersonalMachine [
-      borgbackup
-      ffmpeg
-      go
-      mercurialFull
-      rclone
-      youtube-dl
-    ]) ++ (pkgs.lib.optionals isWorkMachine [
-      stylish-haskell
-      tmux-xpanes
-    ]);
+  paths = pkgs.lib.concatLists (builtins.getAttr hostname {
+    "auburn" = [
+      commonPackages
+      personalPackages
+    ];
+
+    "sienna" = [
+      commonPackages
+      personalPackages
+      linuxPackages
+    ];
+
+    "indigo" = [
+      commonPackages
+      [ pkgs.tmux-xpanes ]
+    ];
+  });
 }
