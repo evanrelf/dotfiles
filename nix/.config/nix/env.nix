e7c97f5cbb04969567805260053bcca08dd81302
Evan Relf <evan@awakesecurity.com>
2021-06-10 14:33:17 -0700

    nix: Indent list of packages

--- nix/.config/nix/env.nix
+++ nix/.config/nix/env.nix
@@ -33,86 +33,87 @@ let
 in
 pkgs.buildEnv {
   name = "env";
-  paths = with pkgs; [
-    (aspellWithDicts (d: with d; [ en en-computers en-science ]))
-    as-tree
-    bashInteractive
-    cabal-install
-    coreutils-gprefix
-    declarative-channels
-    dhall
-    direnv
-    # emacsCustom
-    exa
-    fd
-    findutils-gprefix
-    fish
-    fzf
-    ghcid
-    gitAndTools.delta
-    gitAndTools.gh
-    gitAndTools.gitFull
-    gnugrep-gprefix
-    haskellPackages.cabal-plan
-    haskellPackages.fourmolu
-    helix
-    htop
-    httpie
-    hyperfine
-    iosevka-bin
-    jq
-    kakoune
-    lorri
-    magic-wormhole
-    moreutils
-    neovim
-    nerdfonts
-    nix-diff
-    nix-index
-    nixpkgs-fmt
-    nix-prefetch-git
-    nix-top
-    nix-tree
-    ormoloog
-    ormolu
-    pandoc
-    parinfer-rust
-    perlPackages.GitAutofixup
-    ripgrep
-    rlwrap
-    sd
-    shellcheck
-    starship
-    stylish-haskell
-    tealdeer
-    tmux
-    tmux-thumbs
-    tokei
-    tree
-    watchexec
-    yj
-    zoxide
-  ] ++ (pkgs.lib.optionals isLinux [
-    acpi
-    autocutsel
-    bemenu
-    dmenu # keep for `dmenu_path | bemenu`
-    firefox
-    kitty
-    noto-fonts
-    redshift
-    spotify
-    xbanish
-    xclip
-  ]) ++ (pkgs.lib.optionals isPersonalMachine [
-    borgbackup
-    ffmpeg
-    go
-    mercurialFull
-    rclone
-    youtube-dl
-  ]) ++ (pkgs.lib.optionals isWorkMachine [
-    stylish-haskell
-    tmux-xpanes
-  ]);
+  paths = with pkgs;
+    [
+      (aspellWithDicts (d: with d; [ en en-computers en-science ]))
+      as-tree
+      bashInteractive
+      cabal-install
+      coreutils-gprefix
+      declarative-channels
+      dhall
+      direnv
+      # emacsCustom
+      exa
+      fd
+      findutils-gprefix
+      fish
+      fzf
+      ghcid
+      gitAndTools.delta
+      gitAndTools.gh
+      gitAndTools.gitFull
+      gnugrep-gprefix
+      haskellPackages.cabal-plan
+      haskellPackages.fourmolu
+      helix
+      htop
+      httpie
+      hyperfine
+      iosevka-bin
+      jq
+      kakoune
+      lorri
+      magic-wormhole
+      moreutils
+      neovim
+      nerdfonts
+      nix-diff
+      nix-index
+      nixpkgs-fmt
+      nix-prefetch-git
+      nix-top
+      nix-tree
+      ormoloog
+      ormolu
+      pandoc
+      parinfer-rust
+      perlPackages.GitAutofixup
+      ripgrep
+      rlwrap
+      sd
+      shellcheck
+      starship
+      stylish-haskell
+      tealdeer
+      tmux
+      tmux-thumbs
+      tokei
+      tree
+      watchexec
+      yj
+      zoxide
+    ] ++ (pkgs.lib.optionals isLinux [
+      acpi
+      autocutsel
+      bemenu
+      dmenu # keep for `dmenu_path | bemenu`
+      firefox
+      kitty
+      noto-fonts
+      redshift
+      spotify
+      xbanish
+      xclip
+    ]) ++ (pkgs.lib.optionals isPersonalMachine [
+      borgbackup
+      ffmpeg
+      go
+      mercurialFull
+      rclone
+      youtube-dl
+    ]) ++ (pkgs.lib.optionals isWorkMachine [
+      stylish-haskell
+      tmux-xpanes
+    ]);
 }
