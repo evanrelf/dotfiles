3cc79d85a3c52629283689c248d6a2d67dc95664
Evan Relf <evan@evanrelf.com>
2021-07-02 21:03:18 -0700

    neovim: Move from `init.vim` to `init.lua`

---
+++ neovim/.config/nvim/init.lua
@@ -0,0 +1,11 @@
+require('evan.plugins')
+require('evan.options')
+require('evan.mappings')
+
+vim.api.nvim_exec([[
+  augroup autocmds
+    autocmd!
+    autocmd FileType rust setlocal colorcolumn=81,101
+    autocmd FileType gitcommit setlocal spell colorcolumn=51,73
+  augroup END
+]], false)
--- neovim/.config/nvim/init.vim
+++
@@ -1,9 +0,0 @@
-lua require('evan.plugins')
-lua require('evan.options')
-lua require('evan.mappings')
-
-augroup autocmds
-  autocmd!
-  autocmd FileType rust setlocal colorcolumn=81,101
-  autocmd FileType gitcommit setlocal spell colorcolumn=51,73
-augroup END
