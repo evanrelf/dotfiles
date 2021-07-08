b23cf6ecd79df195816da037673decfb74fa4c8d
Evan Relf <evan@evanrelf.com>
2021-07-07 19:43:45 -0700

    neovim: Use Haskell-style leading commas

--- neovim/.config/nvim/lua/evan/plugins.lua
+++ neovim/.config/nvim/lua/evan/plugins.lua
@@ -1,23 +1,24 @@
-require("paq")({
-  "savq/paq-nvim";
-  "ishan9299/modus-theme-vim";
-  "tpope/vim-sensible";
-  "tpope/vim-repeat";
-  "wellle/targets.vim";
-  "michaeljsmith/vim-indent-object";
-  "sheerun/vim-polyglot";
-  "dense-analysis/ale";
-  "nvim-treesitter/nvim-treesitter";
-  "sbdchd/neoformat";
-  "tmsvg/pear-tree";
-  "tomtom/tcomment_vim";
-  "machakann/vim-sandwich";
-  "junegunn/vim-easy-align";
-  "sickill/vim-pasta";
-  "tpope/vim-sleuth";
-  "bronson/vim-trailing-whitespace";
-  "critiqjo/husk-x.vim";
-})
+require("paq")(
+  { "savq/paq-nvim"
+  , "ishan9299/modus-theme-vim"
+  , "tpope/vim-sensible"
+  , "tpope/vim-repeat"
+  , "wellle/targets.vim"
+  , "michaeljsmith/vim-indent-object"
+  , "sheerun/vim-polyglot"
+  , "dense-analysis/ale"
+  , "nvim-treesitter/nvim-treesitter"
+  , "sbdchd/neoformat"
+  , "tmsvg/pear-tree"
+  , "tomtom/tcomment_vim"
+  , "machakann/vim-sandwich"
+  , "junegunn/vim-easy-align"
+  , "sickill/vim-pasta"
+  , "tpope/vim-sleuth"
+  , "bronson/vim-trailing-whitespace"
+  , "critiqjo/husk-x.vim"
+  }
+)
 
 -- ishan9299/modus-theme-vim
 vim.o.termguicolors = true
@@ -34,27 +35,28 @@ vim.g.ale_lint_on_text_changed = "never"
 vim.g.ale_lint_on_insert_leave = false
 
 -- nvim-treesitter/nvim-treesitter
-require("nvim-treesitter.configs").setup({
-  ensure_installed =
-    { "bash"
-    , "fish"
-    , "haskell"
-    , "html"
-    , "javascript"
-    , "json"
-    , "lua"
-    , "nix"
-    , "python"
-    , "rust"
-    , "toml"
-    , "yaml"
-    },
-  highlight =
-    { enable = true
-    , disable = {"haskell"}
-    },
-  indent = { enable = true },
-})
+require("nvim-treesitter.configs").setup(
+  { ensure_installed =
+      { "bash"
+      , "fish"
+      , "haskell"
+      , "html"
+      , "javascript"
+      , "json"
+      , "lua"
+      , "nix"
+      , "python"
+      , "rust"
+      , "toml"
+      , "yaml"
+      }
+  , highlight =
+      { enable = true
+      , disable = {"haskell"}
+      }
+  , indent = { enable = true }
+  }
+)
 
 -- sbdchd/neoformat
 vim.g.neoformat_only_msg_on_error = true
