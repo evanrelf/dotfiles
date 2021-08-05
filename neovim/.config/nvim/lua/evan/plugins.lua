21de510a5a42afd9f8b24dae3cb674db7eeb8aa7
Evan Relf <evan@awakesecurity.com>
2021-08-05 08:57:10 -0700

    neovim: Switch from `paq` back to `vim-plug`

--- neovim/.config/nvim/lua/evan/plugins.lua
+++ neovim/.config/nvim/lua/evan/plugins.lua
@@ -1,24 +1,26 @@
-require("paq")(
-  { "savq/paq-nvim"
-  , "ishan9299/modus-theme-vim"
-  , "tpope/vim-sensible"
-  , "tpope/vim-repeat"
-  , "wellle/targets.vim"
-  , "michaeljsmith/vim-indent-object"
-  , "sheerun/vim-polyglot"
-  , "dense-analysis/ale"
-  , "nvim-treesitter/nvim-treesitter"
-  , "sbdchd/neoformat"
-  , "tmsvg/pear-tree"
-  , "tomtom/tcomment_vim"
-  , "machakann/vim-sandwich"
-  , "junegunn/vim-easy-align"
-  , "sickill/vim-pasta"
-  , "tpope/vim-sleuth"
-  , "bronson/vim-trailing-whitespace"
-  , "critiqjo/husk-x.vim"
-  }
-)
+vim.api.nvim_exec([[
+  call plug#begin(stdpath('data') . '/plugged')
+
+  Plug 'ishan9299/modus-theme-vim'
+  Plug 'tpope/vim-sensible'
+  Plug 'tpope/vim-repeat'
+  Plug 'wellle/targets.vim'
+  Plug 'michaeljsmith/vim-indent-object'
+  Plug 'sheerun/vim-polyglot'
+  Plug 'dense-analysis/ale'
+  Plug 'nvim-treesitter/nvim-treesitter'
+  Plug 'sbdchd/neoformat'
+  Plug 'tmsvg/pear-tree'
+  Plug 'tomtom/tcomment_vim'
+  Plug 'machakann/vim-sandwich'
+  Plug 'junegunn/vim-easy-align'
+  Plug 'sickill/vim-pasta'
+  Plug 'tpope/vim-sleuth'
+  Plug 'bronson/vim-trailing-whitespace'
+  Plug 'critiqjo/husk-x.vim'
+
+  call plug#end()
+]], false)
 
 -- ishan9299/modus-theme-vim
 vim.o.termguicolors = true
