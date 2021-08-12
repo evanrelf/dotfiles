75198f55fb89cbc18ade2c1d441302efb1d931ce
Evan Relf <evan@awakesecurity.com>
2021-08-12 15:39:33 -0700

    nix: Add `dhall` to all machines

--- nix/.config/nix/env.nix
+++ nix/.config/nix/env.nix
@@ -17,6 +17,7 @@ let
     cabal-install
     declarative-channels
     delta
+    dhall
     exa
     fd
     fish
@@ -45,7 +46,6 @@ let
     as-tree
     bashInteractive
     # coreutils-gprefix
-    dhall
     direnv
     # findutils-gprefix
     gitAndTools.delta
