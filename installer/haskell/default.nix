2f496a92533671c20d9443c31c674647e9cc6d0a
Evan Relf <evan@evanrelf.com>
2021-08-12 22:35:39 -0700

    nix+installer: Add `gitignoreSource` to overlay

--- installer/haskell/default.nix
+++ installer/haskell/default.nix
@@ -1,21 +1,10 @@
 let
   pkgs = import ../../nix/.config/nix/pkgs.nix { };
 
-  gitignoreSource =
-    let
-      source = pkgs.fetchFromGitHub {
-        owner = "hercules-ci";
-        repo = "gitignore.nix";
-        rev = "211907489e9f198594c0eb0ca9256a1949c9d412";
-        sha256 = "06j7wpvj54khw0z10fjyi31kpafkr6hi1k0di13k1xp8kywvfyx8";
-      };
-    in
-    (import source { inherit (pkgs) lib; }).gitignoreSource;
-
   haskellPackage =
     pkgs.haskellPackages.callCabal2nix
       "installer"
-      (gitignoreSource ./.)
+      (pkgs.gitignoreSource ./.)
       { };
 
 in
--- nix/.config/nix/overlays/top-level.nix
+++ nix/.config/nix/overlays/top-level.nix
@@ -93,6 +93,17 @@ in
     });
   };
 
+  gitignoreSource =
+    let
+      source = pkgsPrev.fetchFromGitHub {
+        owner = "hercules-ci";
+        repo = "gitignore.nix";
+        rev = "211907489e9f198594c0eb0ca9256a1949c9d412";
+        sha256 = "06j7wpvj54khw0z10fjyi31kpafkr6hi1k0di13k1xp8kywvfyx8";
+      };
+    in
+    (import source { inherit (pkgsFinal) lib; }).gitignoreSource;
+
   gnugrep-gprefix = gprefix pkgsFinal.gnugrep;
 
   iosevka-bin = pkgsPrev.iosevka-bin.override { variant = "ss08"; };
