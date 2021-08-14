a85392135afef317091376f0a8b6f52a2ce77d0b
Evan Relf <evan@evanrelf.com>
2021-08-13 18:16:07 -0700

    installer: Parse command-line options with `argh`

--- installer/rust/src/main.rs
+++ installer/rust/src/main.rs
@@ -1,3 +1,16 @@
+#[derive(argh::FromArgs, Debug)]
+/// installer
+struct Options {
+    /// packages
+    #[argh(positional)]
+    packages: Vec<String>,
+
+    /// run in dry run mode
+    #[argh(option)]
+    dry_run: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let options: Options = argh::from_env();
+    println!("{:#?}", options);
 }
