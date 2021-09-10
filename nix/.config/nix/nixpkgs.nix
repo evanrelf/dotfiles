dcee7dbbb321ae292f392ac4cbbdb18404db0685
Evan Relf <evan@evanrelf.com>
2021-09-10 16:18:29 -0700

    nix: Bump Nixpkgs

--- nix/.config/nix/nixpkgs.nix
+++ nix/.config/nix/nixpkgs.nix
@@ -2,8 +2,8 @@ args:
 
 let
   # master on 2021-08-12
-  rev = "bbfec00ec916415738ea7b723dd472d7dbafaa18";
-  sha256 = "1v9px4kp9ys3sv5zrdxviw5366rv8fvlkfc7kc72xcdlk1sxjmi0";
+  rev = "6e4c36b3f7ec1400bd92ef42567b687f20f3c3c4";
+  sha256 = "0jq8715g4f8ccmk18df0m0mmq2fdnivaxhx4x61cmkyjps31f868";
 
   nixpkgs = builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
