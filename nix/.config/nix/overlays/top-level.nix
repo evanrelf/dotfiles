8fc4b2ffdeb80e343183ad023b02eb8ed9712f39
Evan Relf <evan@evanrelf.com>
2021-11-08 20:55:57 -0800

    nix: Update `kakoune`

--- nix/.config/nix/overlays/top-level.nix
+++ nix/.config/nix/overlays/top-level.nix
@@ -113,12 +113,12 @@ in
 
   kakoune-unwrapped =
     pkgsPrev.kakoune-unwrapped.overrideAttrs (prev: rec {
-      version = "2021.10.28";
+      version = "2021.11.08";
       src = pkgsPrev.fetchFromGitHub {
         owner = "mawww";
         repo = "kakoune";
         rev = "v${version}";
-        sha256 = "17z536iw6xap2lj3c23j53qnqs5lzr80kvxmdaxmdj07f7mk87d6";
+        sha256 = "02ads7d2scvab9pmsj77hc2kh9fpshmmblmwzw6w26zm86vqrhcl";
       };
       preConfigure = ''
         ${prev.preConfigure}
