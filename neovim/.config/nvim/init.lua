e2d362421891b9c04ad95dedbf99969276e26888
Evan Relf <evan@evanrelf.com>
2021-11-08 20:45:14 -0800

    neovim: Remove `rktjmp/hotpot.nvim` bootstrapping

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,10 +1,3 @@
--- local hotpot_path = vim.fn.stdpath("data") .. "/plugged/hotpot.nvim"
--- if vim.fn.empty(vim.fn.glob(hotpot_path)) > 0 then
---   vim.fn.system({"git", "clone", "https://github.com/rktjmp/hotpot.nvim", hotpot_path})
--- end
--- vim.o.runtimepath = vim.o.runtimepath .. "," .. hotpot_path
--- require("hotpot")
-
 require('evan.plugins')
 require('evan.options')
 require('evan.mappings')
