7408928d3edbef05f01809e125af82dc5f429fad
Evan Relf <evan@evanrelf.com>
2021-11-21 11:50:50 -0800

    nix: Remove Haskell packages overlay

--- nix/.config/nix/pkgs.nix
+++ nix/.config/nix/pkgs.nix
@@ -3,7 +3,6 @@ args:
 import ./nixpkgs.nix ({
   overlays = [
     (import ./overlays/emacs.nix)
-    (import ./overlays/haskell-packages.nix)
     (import ./overlays/kakoune-plugins.nix)
     (import ./overlays/top-level.nix)
   ];
