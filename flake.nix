c0a661fac7aa04170bab0edfa390d8fef19143d6
Evan Relf <evan@evanrelf.com>
2021-11-21 19:49:31 -0800

    Start using `home-manager` again

---
+++ flake.nix
@@ -0,0 +1,78 @@
+{
+  description = "evanrelf's dotfiles";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }:
+    let
+      eachSystem = f:
+        builtins.listToAttrs
+          (builtins.map
+            (system: { name = system; value = f system; })
+            [
+              "aarch64-darwin"
+              "x86_64-darwin"
+              "x86_64-linux"
+            ]);
+    in
+    {
+      # e.g. `nix run . -- switch`
+      defaultApp = eachSystem (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+
+        in
+        {
+          type = "app";
+          program = builtins.toString (pkgs.writeShellScript "home-manager" ''
+            export PATH="${home-manager.defaultPackage."${system}"}/bin:$PATH"
+            hostname=$(hostname -s)
+            { set -x; home-manager --flake .#$hostname $@; }
+          '');
+        }
+      );
+
+      homeConfigurations = rec {
+        "ultraviolet" = home-manager.lib.homeManagerConfiguration rec {
+          system = "aarch64-darwin";
+          username = "evanrelf";
+          homeDirectory = "/Users/${username}";
+          configuration.imports = [ ];
+        };
+
+        "auburn" = home-manager.lib.homeManagerConfiguration rec {
+          system = "x86_64-darwin";
+          username = "evanrelf";
+          homeDirectory = "/Users/${username}";
+          configuration.imports = [ ];
+        };
+
+        "sienna" = home-manager.lib.homeManagerConfiguration rec {
+          system = "x86_64-linux";
+          username = "evanrelf";
+          homeDirectory = "/home/${username}";
+          configuration.imports = [ ];
+        };
+
+        "indigo" = home-manager.lib.homeManagerConfiguration rec {
+          system = "x86_64-darwin";
+          username = "evan";
+          homeDirectory = "/Users/${username}";
+          configuration.imports = [ ];
+        };
+
+        "hydra-dev" = home-manager.lib.homeManagerConfiguration rec {
+          system = "x86_64-linux";
+          username = "evan";
+          homeDirectory = "/home/${username}";
+          configuration.imports = [ ];
+        };
+      };
+    };
+}
