d62c59cdd93fca9ea53f5343ce0b40eef6b30a21
Evan Relf <evan@evanrelf.com>
2021-11-27 19:33:38 -0800

    Automatically pick up new modules

--- modules/default.nix
+++ modules/default.nix
@@ -1,24 +1,10 @@
-[
-  ./programs/alacritty.nix
-  ./programs/borg-backup.nix
-  ./programs/doom-emacs.nix
-  ./programs/emacs.nix
-  ./programs/fish.nix
-  ./programs/git.nix
-  ./programs/hammerspoon.nix
-  ./programs/helix.nix
-  ./programs/kakoune.nix
-  ./programs/karabiner.nix
-  ./programs/kitty.nix
-  ./programs/mercurial.nix
-  ./programs/neovim.nix
-  ./programs/nix.nix
-  ./programs/skhd.nix
-  ./programs/starship.nix
-  ./programs/sway.nix
-  ./programs/tmux.nix
-  ./programs/vis.nix
-  ./programs/wezterm.nix
-  ./programs/xmonad.nix
-  ./programs/yabai.nix
+let
+  listDir = dir:
+    builtins.map
+      (path: dir + "/${path}")
+      (builtins.attrNames (builtins.readDir dir));
+
+in
+builtins.concatMap listDir [
+  ./programs
 ]
