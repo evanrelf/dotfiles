bb048ac72dcac899151ce83a8888dca91f90971d
Evan Relf <evan@evanrelf.com>
2021-11-27 16:42:55 -0800

    fish: Add `n` as abbreviation for `nix`

--- configs/fish/.config/fish/aliases.fish
+++ configs/fish/.config/fish/aliases.fish
@@ -62,6 +62,7 @@ end
 alias utcdate "date -u +'%Y-%m-%dT%H:%M:%S%Z'"
 
 if _exists nix
+    abbr --add n "nix"
     alias nix-death-row "sudo tree -x -C --prune / | grep -vE '(/etc/static|/persist|/nix/store)' | less"
 end
 
