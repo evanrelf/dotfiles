100a2653ba7e7d204f6ce04d012d6d317044b60f
Evan Relf <evan@evanrelf.com>
2021-11-30 19:16:17 -0800

    emacs: Add more essential configuration

--- configs/emacs/.config/emacs/init.el
+++ configs/emacs/.config/emacs/init.el
@@ -20,3 +20,40 @@
   (setq evil-want-keybinding nil)
   :config (evil-mode +1))
 
+(leaf modus-themes
+  :ensure t
+  :config (load-theme 'modus-vivendi t))
+
+;; Disable user interface elements
+(menu-bar-mode -1)
+(when (display-graphic-p)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
+  (horizontal-scroll-bar-mode -1)
+  (blink-cursor-mode -1)
+  (tooltip-mode -1)
+  (fringe-mode -1))
+
+;; Disable startup messages
+(setq inhibit-startup-screen t)
+(setq initial-scratch-message nil)
+(fset 'display-startup-echo-area-message 'ignore)
+
+;; Disable audio bell
+(setq ring-bell-function 'ignore)
+
+;; Disable clutter files
+(setq custom-file "/dev/null")
+(setq make-backup-files nil)
+(setq create-lockfiles nil)
+(setq auto-save-default nil)
+
+;; Shorten yes/no prompts to y/n
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; Follow symlinks when opening files under version control
+(setq vc-follow-symlinks t)
+
+;; Indent with 2 spaces
+(setq tab-width 2)
+(setq indent-tabs-mode nil)
