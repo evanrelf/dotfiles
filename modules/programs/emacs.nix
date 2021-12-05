a3de3d9a1df2d5d01e878d8c5e8822b24ea5aa41
Evan Relf <evan@evanrelf.com>
2021-12-05 09:28:39 -0800

    Create working, always up-to-date `Emacs.app`
    By linking the guts rather than the whole thing, I can have a stable
    `.app` bundle recognized by Finder, Alfred, etc. but always use the
    latest produced by Nix.

--- modules/programs/emacs.nix
+++ modules/programs/emacs.nix
@@ -11,44 +11,55 @@ in
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    assertions = [
-      {
-        assertion = !config.dotfiles.programs.doom-emacs.enable;
-        message = "emacs: dotfiles.programs.{doom-emacs,emacs} cannot be enabled simultaneously";
-      }
-    ];
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      assertions = [
+        {
+          assertion = !config.dotfiles.programs.doom-emacs.enable;
+          message = "emacs: dotfiles.programs.{doom-emacs,emacs} cannot be enabled simultaneously";
+        }
+      ];
 
-    home.packages = [ pkgs.emacsCustom ];
+      home.packages = [ pkgs.emacsCustom ];
 
-    home.file.".local/bin" = {
-      source = ../../configs/emacs/.local/bin;
-      recursive = true;
-    };
+      home.file.".local/bin" = {
+        source = ../../configs/emacs/.local/bin;
+        recursive = true;
+      };
 
-    # xdg.configFile."emacs" = {
-    #   source = ../../configs/emacs/.config/emacs;
-    #   recursive = true;
-    # };
+      # xdg.configFile."emacs" = {
+      #   source = ../../configs/emacs/.config/emacs;
+      #   recursive = true;
+      # };
 
-    home.activation.emacsLinkConfig =
-      lib.hm.dag.entryAfter [ "writeBoundary" "linkGeneration" ] ''
-        if [ ! -e "$HOME/.config/emacs/init.el" ]; then
-          $DRY_RUN_CMD mkdir -p "$HOME/.config/emacs"
-          $DRY_RUN_CMD ln -s {$OLDPWD/configs/emacs,$HOME}/.config/emacs/init.el
-        fi
-      '';
+      home.activation.emacsLinkConfig =
+        lib.hm.dag.entryAfter [ "writeBoundary" "linkGeneration" ] ''
+          if [ ! -e "$HOME/.config/emacs/init.el" ]; then
+            $DRY_RUN_CMD mkdir -p "$HOME/.config/emacs"
+            $DRY_RUN_CMD ln -s {$OLDPWD/configs/emacs,$HOME}/.config/emacs/init.el
+          fi
+        '';
 
-    home.activation.emacsCleanupImperativePackages =
-      lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        $DRY_RUN_CMD rm -rf "$HOME"/.config/emacs/{eln-cache,elpa}
-      '';
+      home.activation.emacsCleanupImperativePackages =
+        lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+          $DRY_RUN_CMD rm -rf "$HOME"/.config/emacs/{eln-cache,elpa}
+        '';
 
-    home.activation.emacsTruecolor =
-      lib.hm.dag.entryAfter [ "writeBoundary" "linkGeneration" ] ''
-        if [ ! -e "$HOME/.local/share/terminfo" ]; then
-          $DRY_RUN_CMD emacs-setup-truecolor
-        fi
-      '';
-  };
+      home.activation.emacsTruecolor =
+        lib.hm.dag.entryAfter [ "writeBoundary" "linkGeneration" ] ''
+          if [ ! -e "$HOME/.local/share/terminfo" ]; then
+            $DRY_RUN_CMD emacs-setup-truecolor
+          fi
+        '';
+    })
+    (lib.mkIf pkgs.stdenv.isDarwin {
+      home.activation.emacsLinkMacApp =
+        lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+          if [ ! -e "/Applications/Emacs.app" ]; then
+            $DRY_RUN_CMD mkdir -p "/Applications/Emacs.app"
+            $DRY_RUN_CMD ln -s "$HOME/.nix-profile/Applications/Emacs.app/Contents" "/Applications/Emacs.app/Contents"
+          fi
+        '';
+    })
+  ];
 }
