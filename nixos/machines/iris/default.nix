19caf8ba4108ce981932e84407987e0eff78ee3d
Evan Relf <evan@evanrelf.com>
2022-01-06 21:16:47 -0800

    iris: Set up Prometheus and Grafana for monitoring

--- nixos/machines/iris/default.nix
+++ nixos/machines/iris/default.nix
@@ -5,6 +5,7 @@
   imports = [
     "${modulesPath}/installer/scan/not-detected.nix"
     ./time-machine.nix
+    ./monitoring.nix
   ];
   boot.initrd.availableKernelModules =
     [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sr_mod" ];
