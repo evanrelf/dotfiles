51af580f3acf69e0b2a28b379d1f03caf157cfb5
Evan Relf <evan@evanrelf.com>
2022-01-06 18:56:12 -0800

    iris: Persist Samba user passwords

--- nixos/machines/iris/default.nix
+++ nixos/machines/iris/default.nix
@@ -108,6 +108,7 @@
       "/etc/ssh/ssh_host_ed25519_key.pub"
       "/etc/ssh/ssh_host_rsa_key"
       "/etc/ssh/ssh_host_rsa_key.pub"
+      "/var/lib/samba/private/passdb.tdb"
       "/var/lib/tailscale/tailscaled.state"
     ];
   };
