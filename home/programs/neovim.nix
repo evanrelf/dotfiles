3425ad9f7d1e694e1c7495c8306471de6f45dfc8
Evan Relf <evan@awakesecurity.com>
2022-01-28 12:00:46 -0800

    Don't auto-install Neovim plugins

--- home/programs/neovim.nix
+++ home/programs/neovim.nix
@@ -21,16 +21,5 @@ in
 
     xdg.dataFile."nvim/site/autoload/plug.vim".source =
       "${inputs.vim-plug}/plug.vim";
-
-    home.activation.neovimInstallPlugins =
-      lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        if [ ! -d "$HOME/.local/share/nvim/plugged" ]; then
-          echo "Installing plugins"
-          $DRY_RUN_CMD nvim \
-            -u "$HOME/.config/nvim/lua/evan/plug.lua" \
-            -i NONE \
-            -c "PlugUpdate" -c "PlugClean!" -c "qa"
-        fi
-      '';
   };
 }
