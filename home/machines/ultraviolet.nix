5b7cb595f5724b7bd36f722fb26388ce6cb380bf
Evan Relf <evan@evanrelf.com>
2022-03-03 23:34:10 -0800

    git: Configure basic commit settings

--- home/machines/ultraviolet.nix
+++ home/machines/ultraviolet.nix
@@ -2,10 +2,16 @@
 
 {
   home.packages = with pkgs; [
+    git
     home-manager
     neovim
     zsh
   ];
+
+  xdg.configFile."git" = {
+    source = ../../configs/git/.config/git;
+    recursive = true;
+  };
 
   home.file.".zshrc".source = ../../configs/zsh/.zshrc;
 }
