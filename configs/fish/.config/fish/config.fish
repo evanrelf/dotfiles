ffb5ad9a38f3caa90d13d1bc4afd432494268972
Evan Relf <evan@evanrelf.com>
2022-03-05 14:16:01 -0800

    fish: Fix incorrect `fd` commands
    - Was using `--type file` instead of `--type directory` with the
      `cd`-related commands
    
    - Wasn't ending commands with `. \$dir`

--- configs/fish/.config/fish/config.fish
+++ configs/fish/.config/fish/config.fish
@@ -2,14 +2,11 @@ set --global --export --prepend PATH "/nix/var/nix/profiles/default/bin"
 set --global --export --prepend PATH "$HOME/.nix-profile/bin"
 set --global --export COLORTERM "$TERM"
 set --global --export EDITOR "nvim"
-begin
-    set --universal FZF_LEGACY_KEYBINDINGS 0
-    set --universal FZF_DEFAULT_OPTS "--color=light --height=40% --layout=reverse --exact"
-    set --local fzf_default_command "fd --type file --follow --exclude '.git'"
-    set --universal FZF_CD_COMMAND "$fzf_default_command"
-    set --universal FZF_CD_WITH_HIDDEN_COMMAND "$fzf_default_command --hidden"
-    set --universal FZF_FIND_FILE_COMMAND "$fzf_default_command --hidden"
-    set --universal FZF_OPEN_COMMAND "$fzf_default_command --hidden"
-end
+set --universal FZF_LEGACY_KEYBINDINGS 0
+set --universal FZF_DEFAULT_OPTS "--color=light --height=40% --layout=reverse --exact"
+set --universal FZF_CD_COMMAND "fd --type directory --follow --exclude '.git' . \$dir"
+set --universal FZF_CD_WITH_HIDDEN_COMMAND "fd --type directory --follow --exclude '.git' --hidden . \$dir"
+set --universal FZF_FIND_FILE_COMMAND "fd --type file --follow --exclude '.git' --hidden . \$dir"
+set --universal FZF_OPEN_COMMAND "fd --type file --follow --exclude '.git' --hidden . \$dir"
 alias ls "ls --color=auto"
 set --global fish_greeting ""
