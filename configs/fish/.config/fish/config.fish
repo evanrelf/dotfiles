8cb24f9be87b3180448c79d86414d255b5234150
Evan Relf <evan@evanrelf.com>
2022-03-05 13:39:29 -0800

    fish: Always specify scope for variables

--- configs/fish/.config/fish/config.fish
+++ configs/fish/.config/fish/config.fish
@@ -1,4 +1,4 @@
-set --export --prepend PATH "/nix/var/nix/profiles/default/bin"
-set --export --prepend PATH "$HOME/.nix-profile/bin"
-set --export COLORTERM "$TERM"
+set --global --export --prepend PATH "/nix/var/nix/profiles/default/bin"
+set --global --export --prepend PATH "$HOME/.nix-profile/bin"
+set --global --export COLORTERM "$TERM"
 alias ls "command ls --color=auto"
