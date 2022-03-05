82242ba4bf73fbfb7d1aa8f236775a3150fecefd
Evan Relf <evan@evanrelf.com>
2022-03-05 14:05:14 -0800

    fish: Use `fd` with `fzf`

--- configs/fish/.config/fish/config.fish
+++ configs/fish/.config/fish/config.fish
@@ -1,6 +1,13 @@
 set --global --export --prepend PATH "/nix/var/nix/profiles/default/bin"
 set --global --export --prepend PATH "$HOME/.nix-profile/bin"
 set --global --export COLORTERM "$TERM"
-set --universal FZF_LEGACY_KEYBINDINGS 0
-set --universal FZF_DEFAULT_OPTS "--color=light --height=40% --layout=reverse --exact"
+begin
+    set --universal FZF_LEGACY_KEYBINDINGS 0
+    set --universal FZF_DEFAULT_OPTS "--color=light --height=40% --layout=reverse --exact"
+    set --local fzf_default_command "fd --type file --follow --exclude '.git'"
+    set --universal FZF_CD_COMMAND "$fzf_default_command"
+    set --universal FZF_CD_WITH_HIDDEN_COMMAND "$fzf_default_command --hidden"
+    set --universal FZF_FIND_FILE_COMMAND "$fzf_default_command --hidden"
+    set --universal FZF_OPEN_COMMAND "$fzf_default_command --hidden"
+end
 alias ls "command ls --color=auto"
