a20ec14da03b2c296bf40deebcd2d64178caab05
Evan Relf <evan@evanrelf.com>
2022-03-05 13:42:04 -0800

    fish: Install `fzf` plugin

--- configs/fish/.config/fish/config.fish
+++ configs/fish/.config/fish/config.fish
@@ -1,4 +1,6 @@
 set --global --export --prepend PATH "/nix/var/nix/profiles/default/bin"
 set --global --export --prepend PATH "$HOME/.nix-profile/bin"
 set --global --export COLORTERM "$TERM"
+set --universal FZF_LEGACY_KEYBINDINGS 0
+set --universal FZF_DEFAULT_OPTS "--color=light --height=40% --layout=reverse --exact"
 alias ls "command ls --color=auto"
