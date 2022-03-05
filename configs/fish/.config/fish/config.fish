c92802db9d8be2ecd0d096cddc522b3ee9ba5482
Evan Relf <evan@evanrelf.com>
2022-03-05 14:05:50 -0800

    fish: Set `EDITOR` to `nvim`

--- configs/fish/.config/fish/config.fish
+++ configs/fish/.config/fish/config.fish
@@ -1,6 +1,7 @@
 set --global --export --prepend PATH "/nix/var/nix/profiles/default/bin"
 set --global --export --prepend PATH "$HOME/.nix-profile/bin"
 set --global --export COLORTERM "$TERM"
+set --global --export EDITOR "nvim"
 begin
     set --universal FZF_LEGACY_KEYBINDINGS 0
     set --universal FZF_DEFAULT_OPTS "--color=light --height=40% --layout=reverse --exact"
