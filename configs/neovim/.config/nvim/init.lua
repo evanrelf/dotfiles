12207df944bfc3dbd1f02d6b8b32ace034f39f7b
Evan Relf <evan@evanrelf.com>
2022-03-10 23:00:11 -0800

    neovim: Install `wbthomason/packer.nvim`

--- configs/neovim/.config/nvim/init.lua
+++ configs/neovim/.config/nvim/init.lua
@@ -1,3 +1,18 @@
+vim.cmd("packadd packer.nvim")
+require("packer").startup(function(use)
+  use({
+    "wbthomason/packer.nvim",
+    config = function()
+      vim.api.nvim_exec([[
+        augroup packer
+          autocmd!
+          autocmd BufWritePost init.lua source <afile> | PackerCompile
+        augroup end
+      ]], false)
+    end,
+  })
+end)
+
 vim.o.shiftwidth = 2
 vim.bo.expandtab = true
 vim.o.mouse = "a"
