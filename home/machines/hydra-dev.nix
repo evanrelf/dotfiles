36b3e2bdb40e76b0a36d8d55d458127990fd70a0
Evan Relf <evan@awakesecurity.com>
2022-05-09 11:35:16 -0700

    Add `hydra-dev`-specific customizations

--- home/machines/hydra-dev.nix
+++ home/machines/hydra-dev.nix
@@ -1,7 +1,31 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [
     ./common.nix
   ];
+
+  home.packages = with pkgs; [
+    gcc
+    gnupg
+    pinentry.tty
+  ];
+
+  home.file.".bash_profile".text = ''
+    # Use `fish` as my shell without needing to modify `/etc/shells`
+    shopt -q login_shell && exec fish --login
+  '';
+
+  xdg.configFile."fish/local.fish".text = ''
+    set --export GPG_TTY (tty)
+  '';
+
+  home.file.".gnupg/gpg-agent.conf" = {
+    text = ''
+      pinentry-program /home/evan/.nix-profile/bin/pinentry
+    '';
+    onChange = ''
+      chmod 700 "$HOME"/.gnupg
+    '';
+  };
 }
