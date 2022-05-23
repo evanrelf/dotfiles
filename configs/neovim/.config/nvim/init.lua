6e2da8a8d7516103967304993cb09711f45161f2
Evan Relf <evan@evanrelf.com>
2022-05-23 10:35:33 -0700

    neovim: Add `linty-org/readline.nvim`

--- configs/neovim/.config/nvim/init.lua
+++ configs/neovim/.config/nvim/init.lua
@@ -170,6 +170,23 @@ require("packer").startup(function(use)
     end,
   })
 
+  use({
+    "linty-org/readline.nvim",
+    config = function()
+      local readline = require("readline")
+      vim.keymap.set("!", "<C-n>", "<Down>")
+      vim.keymap.set("!", "<C-p>", "<Up>")
+      vim.keymap.set("!", "<C-f>", "<Right>")
+      vim.keymap.set("!", "<C-b>", "<Left>")
+      vim.keymap.set("!", "<M-f>", readline.forward_word)
+      vim.keymap.set("!", "<M-b>", readline.backward_word)
+      vim.keymap.set("!", "<C-a>", readline.beginning_of_line)
+      vim.keymap.set("!", "<C-e>", readline.end_of_line)
+      vim.keymap.set("!", "<C-k>", readline.kill_line)
+      vim.keymap.set("!", "<C-u>", readline.backward_kill_line)
+    end,
+  })
+
   use({ "machakann/vim-sandwich" })
 
   use({ "michaeljsmith/vim-indent-object" })
