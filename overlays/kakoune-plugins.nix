87803d8731db58922697e4c08148ed90b162bd42
Evan Relf <evan@awakesecurity.com>
2022-06-02 17:09:30 -0700

    kakoune: Alphabetize plugins

--- overlays/kakoune-plugins.nix
+++ overlays/kakoune-plugins.nix
@@ -2,11 +2,11 @@ pkgsFinal: pkgsPrev:
 
 let
   sources = {
-    "primer-kak" = {
+    "better-haskell-kak" = {
       owner = "evanrelf";
-      repo = "primer.kak";
-      rev = "4c0c1c48de189e1f1a8582c57d89f8008b358430";
-      sha256 = "0767nbxhkpl9izz5hyzw7ifsi5z657813gisl3q5dpr0nxyp6v3q";
+      repo = "better-haskell.kak";
+      rev = "b9524dd43e13268f7a9517c177b2d6062b1a3708";
+      sha256 = "sha256-xz0UUDSlkx0hq14SnoiajuZJQBsjfV+jE2GEk2oxRDk=";
     };
 
     "byline-kak" = {
@@ -16,11 +16,11 @@ let
       sha256 = "0yxialy0n35hw2wcnpacax1b19jqwbm7i1kq66x7g4msadlqqigj";
     };
 
-    "better-haskell-kak" = {
+    "primer-kak" = {
       owner = "evanrelf";
-      repo = "better-haskell.kak";
-      rev = "b9524dd43e13268f7a9517c177b2d6062b1a3708";
-      sha256 = "sha256-xz0UUDSlkx0hq14SnoiajuZJQBsjfV+jE2GEk2oxRDk=";
+      repo = "primer.kak";
+      rev = "4c0c1c48de189e1f1a8582c57d89f8008b358430";
+      sha256 = "0767nbxhkpl9izz5hyzw7ifsi5z657813gisl3q5dpr0nxyp6v3q";
     };
   };
 
