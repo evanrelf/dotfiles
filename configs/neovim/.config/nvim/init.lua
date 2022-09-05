babd35242b0c25fafe8c0949916ae52b92e9328f
Evan Relf <evan@evanrelf.com>
2022-09-04 22:42:29 -0700

    neovim: Remove `nvim-telescope/telescope.nvim`

--- configs/neovim/.config/nvim/init.lua
+++ configs/neovim/.config/nvim/init.lua
@@ -161,31 +161,6 @@ require("packer").startup(function(use)
   })
 
   use({
-    "nvim-telescope/telescope.nvim",
-    requires = {
-      "nvim-lua/plenary.nvim",
-      { "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
-    },
-    config = function()
-      local telescope = require("telescope")
-      local telescope_builtin = require("telescope.builtin")
-      telescope.setup({
-        defaults = {
-          preview = false,
-        },
-        pickers = {
-          buffers = { theme = "ivy" },
-          find_files = { theme = "ivy" },
-        },
-      })
-      telescope.load_extension("fzf")
-      telescope.load_extension("ghc")
-      vim.keymap.set("n", "<Leader>f", telescope_builtin.find_files)
-      vim.keymap.set("n", "<Leader>b", telescope_builtin.buffers)
-    end,
-  })
-
-  use({
     "nvim-treesitter/nvim-treesitter",
     config = function()
         -- stylua: ignore
