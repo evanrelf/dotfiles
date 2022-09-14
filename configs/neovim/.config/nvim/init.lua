29f755a7ef9c166f18acbc8d1d904d6a696384cf
Evan Relf <evanr@mercury.com>
2022-09-14 16:00:47 -0700

    neovim: Get completions from LSP

--- configs/neovim/.config/nvim/init.lua
+++ configs/neovim/.config/nvim/init.lua
@@ -4,12 +4,17 @@ require("packer").startup(function(use)
 
   use({
     "hrsh7th/nvim-cmp",
-    requires = { "hrsh7th/cmp-buffer", "hrsh7th/cmp-path" },
+    requires = {
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
+    },
     config = function()
       local has_words_before = function()
         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
         return col ~= 0
-          and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
+          and vim.api
+              .nvim_buf_get_lines(0, line - 1, line, true)[1]
               :sub(col, col)
               :match("%s")
             == nil
@@ -17,6 +22,7 @@ require("packer").startup(function(use)
       local cmp = require("cmp")
       cmp.setup({
         sources = {
+          { name = "nvim_lsp", max_item_count = 10 },
           { name = "buffer" },
           { name = "path" },
         },
@@ -169,8 +175,12 @@ require("packer").startup(function(use)
     "neovim/nvim-lspconfig",
     after = { "nvim-cmp" },
     config = function()
+      local capabilities = require("cmp_nvim_lsp").update_capabilities(
+        vim.lsp.protocol.make_client_capabilities()
+      )
       require("lspconfig").hls.setup({
         autostart = false,
+        capabilities = capabilities,
       })
     end,
   })
