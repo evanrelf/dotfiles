1db62011e1e08c080fc37220efad139e6bf9357c
Evan Relf <evan@evanrelf.com>
2022-09-17 22:29:43 -0700

    wezterm: Disable font ligatures

--- configs/wezterm/.config/wezterm/wezterm.lua
+++ configs/wezterm/.config/wezterm/wezterm.lua
@@ -1,8 +1,8 @@
 local wezterm = require("wezterm")
 
 local font = wezterm.font_with_fallback({
-  "PragmataPro Liga",
-  "Iosevka SS08",
+  { family = "PragmataPro" },
+  { family = "Iosevka SS08", harfbuzz_features = { "calt=0" } },
   "Iosevka Nerd Font",
 })
 
