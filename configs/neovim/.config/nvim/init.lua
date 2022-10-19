c61a47cea85002e46963e0c78d3bda5455d31ad7
Evan Relf <evanr@mercury.com>
2022-10-19 15:08:32 -0700

    neovim: Add `akinsho/bufferline.nvim`

--- configs/neovim/.config/nvim/init.lua
+++ configs/neovim/.config/nvim/init.lua
@@ -1,6 +1,22 @@
 vim.cmd("packadd packer.nvim")
 require("packer").startup(function(use)
   use({
+    "akinsho/bufferline.nvim",
+    tag = "v3.*",
+    config = function()
+      require("bufferline").setup({
+        options = {
+          diagnostics = "nvim_lsp",
+          show_buffer_icons = false,
+          show_buffer_close_icons = false,
+          show_close_icon = false,
+          always_show_bufferline = false,
+        },
+      })
+    end,
+  })
+
+  use({
     "ggandor/flit.nvim",
     requires = { "ggandor/leap.nvim" },
     config = function()
