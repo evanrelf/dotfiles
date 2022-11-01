fc0765d25abd0a4b5caa94391a5e8b56cf877309
Evan Relf <evan@evanrelf.com>
2022-10-31 23:34:53 -0700

    Add `nix-community` substituter to `nixConfig`

--- flake.nix
+++ flake.nix
@@ -51,4 +51,9 @@
         packages = pkgs;
       }
     );
+
+  nixConfig = {
+    extra-substituters = "https://nix-community.cachix.org";
+    extra-trusted-public-keys = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
+  };
 }
