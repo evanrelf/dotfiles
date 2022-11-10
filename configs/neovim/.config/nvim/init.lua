c389c8679f630dcc36ba14468e7904febe197eaf
Evan Relf <evanr@mercury.com>
2022-11-10 12:08:16 -0800

    neovim: Show bufferline close buttons

--- configs/neovim/.config/nvim/init.lua
+++ configs/neovim/.config/nvim/init.lua
@@ -8,7 +8,6 @@ require("packer").startup(function(use)
         options = {
           diagnostics = "nvim_lsp",
           show_buffer_icons = false,
-          show_buffer_close_icons = false,
           show_close_icon = false,
           always_show_bufferline = false,
         },
