beeb09f4f8b8c6ebf8df8f072b655b6f085b1b34
Evan Relf <evanr@mercury.com>
2023-02-16 16:40:32 -0800

    neovim: Remove dead code in Haskell snippets file

--- configs/neovim/.config/nvim/snippets/haskell.lua
+++ configs/neovim/.config/nvim/snippets/haskell.lua
@@ -1,14 +1,7 @@
 local luasnip = require("luasnip")
 local s = luasnip.s
 local i = luasnip.insert_node
-local f = luasnip.function_node
 local fmt = require("luasnip.extras.fmt").fmt
-
-local same = function(index)
-  return f(function(args)
-    return args[1]
-  end, { index })
-end
 
 local auto = function(regex, nodes)
   local trigger = { trig = regex, regTrig = true }
