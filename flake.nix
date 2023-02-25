1d90ec3d4faab6055387519e2a9fcb8da01a6d6b
Evan Relf <evan@evanrelf.com>
2023-02-25 01:25:22 -0800

    Add `evanrelf` Cachix cache to `flake.nix`

--- flake.nix
+++ flake.nix
@@ -75,7 +75,7 @@
     );
 
   nixConfig = {
-    extra-substituters = "https://nix-community.cachix.org";
-    extra-trusted-public-keys = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
+    extra-substituters = "https://nix-community.cachix.org https://evanrelf.cachix.org";
+    extra-trusted-public-keys = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= evanrelf.cachix.org-1:n9mrgldEeLIlie/UEGulvshb2Yf5bxz1ZYUIvV5kdO4=";
   };
 }
