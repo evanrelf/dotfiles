5022821b1f1cf7b0abcbdc77481425eba2489798
Evan Relf <evan@evanrelf.com>
2023-03-01 21:18:11 -0800

    emacs: Use `PragmataPro` font for all text

--- configs/emacs/.config/doom/config.el
+++ configs/emacs/.config/doom/config.el
@@ -20,8 +20,7 @@
 ;;
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
-(setq doom-font (font-spec :family "PragmataPro" :size 16)
-      doom-variable-pitch-font (font-spec :family "PragmataPro" :size 16))
+(setq doom-font (font-spec :family "PragmataPro" :size 16))
 
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
