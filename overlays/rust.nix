18c525be2fce394d1525568234cb4b1394ebfc13
Evan Relf <evan@evanrelf.com>
2023-08-01 20:20:02 -0700

    Update Rust overlay to reflect no more workspace

--- overlays/rust.nix
+++ overlays/rust.nix
@@ -1,42 +1,16 @@
 pkgsFinal: pkgsPrev:
 
-let
-  src = ../src/rust;
-
-  crane =
-    pkgsPrev.crane.overrideScope' (craneFinal: cranePrev: {
-      mkCargoDerivation = args:
-        (pkgsPrev.crane.mkCargoDerivation args).overrideAttrs (prev: {
-          nativeBuildInputs = (prev.nativeBuildInputs or [ ]) ++
-            pkgsPrev.lib.optionals pkgsPrev.stdenv.isDarwin [
-              pkgsFinal.darwin.apple_sdk.frameworks.Security
-              pkgsFinal.libiconv
-            ];
-        });
-    });
-
-  cargoArtifacts =
-    crane.buildDepsOnly {
-      pname = "cargoArtifacts";
-      version = "0.0.0";
-      inherit src;
-    };
-
-in
 {
   comma-update =
-    crane.buildPackage {
-      pname = "comma-update";
-      version = "0.0.0";
-      inherit src cargoArtifacts;
-      cargoBuildCommand = "cargo build --release --bin comma-update";
+    pkgsFinal.crane.buildPackage {
+      src = ../src/rust/comma-update;
+      nativeBuildInputs = [
+        pkgsFinal.darwin.apple_sdk.frameworks.Security
+      ];
     };
 
   home-rebuild =
-    crane.buildPackage {
-      pname = "home-rebuild";
-      version = "0.0.0";
-      inherit src cargoArtifacts;
-      cargoBuildCommand = "cargo build --release --bin home-rebuild";
+    pkgsFinal.crane.buildPackage {
+      src = ../src/rust/home-rebuild;
     };
 }
