873f3df07e38f2a0c2df241e3f23494ec338ec21
Evan Relf <evan@evanrelf.com>
2023-09-29 18:19:35 -0700

    fish: Stop swallowing exit codes in pipelines

--- configs/fish/.config/fish/config.fish
+++ configs/fish/.config/fish/config.fish
@@ -100,6 +100,7 @@ abbr --add !! --position anywhere --function last_history_item
 function rg
     if isatty stdout
         command rg --pretty $argv | command less -RMFXK
+        return $pipestatus[1]
     else
         command rg $argv
     end
