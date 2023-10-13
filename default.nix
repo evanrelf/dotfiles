2ddfebeace89e90ab487b7f3f20588c1c31effd6
Evan Relf <evan@evanrelf.com>
2023-10-12 23:14:23 -0700

    Use updated `flake-compat` config

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@ let
 
   flake-compat =
     builtins.fetchTarball {
-      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      url = lock.nodes.flake-compat.locked.url or "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
       sha256 = lock.nodes.flake-compat.locked.narHash;
     };
 
