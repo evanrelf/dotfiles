a6f34a9902a63b7f0c66ce71b3ef8037987cfeb7
Evan Relf <evan@evanrelf.com>
2023-11-20 08:57:21 -0800

    Switch to `flake-parts`

--- flake.nix
+++ flake.nix
@@ -6,7 +6,6 @@
       url = "github:nix-community/comma";
       inputs.flake-compat.follows = "flake-compat";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.utils.follows = "flake-utils";
     };
     crane = {
       url = "github:ipetkov/crane";
@@ -20,7 +19,7 @@
       url = "github:edolstra/flake-compat";
       flake = false;
     };
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-overlay.url = "github:evanrelf/haskell-overlay";
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -31,44 +30,46 @@
       flake = false;
     };
     nixpkgs.url = "github:NixOS/nixpkgs";
+    systems.url = "github:nix-systems/default";
   };
 
-  outputs = inputs@{ flake-utils, nixpkgs, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        localSystem = system;
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
 
-        config = { };
+      perSystem = { config, pkgs, system, ... }: {
+        _module.args.pkgs =
+          import inputs.nixpkgs {
+            localSystem = system;
+            config = { };
+            overlays = [
+              (_: _: { inherit inputs; })
+              (_: _: { crane = inputs.crane.lib.${system}; })
+              inputs.fenix.overlays.default
+              inputs.comma.overlays.default
+              inputs.haskell-overlay.overlays.default
+              (import ./overlays/evan.nix)
+              (import ./overlays/haskell.nix)
+              (import ./overlays/rust.nix)
+              (import ./overlays/packages.nix)
+              (import ./overlays/kakoune-plugins.nix)
+              (import ./overlays/home-configurations.nix)
+              (import ./overlays/fonts.nix)
+            ];
+          };
 
-        overlays = [
-          (_: _: { inherit inputs; })
-          (_: _: { crane = inputs.crane.lib.${system}; })
-          inputs.fenix.overlays.default
-          inputs.comma.overlays.default
-          inputs.haskell-overlay.overlays.default
-          (import ./overlays/evan.nix)
-          (import ./overlays/haskell.nix)
-          (import ./overlays/rust.nix)
-          (import ./overlays/packages.nix)
-          (import ./overlays/kakoune-plugins.nix)
-          (import ./overlays/home-configurations.nix)
-          (import ./overlays/fonts.nix)
-        ];
-
-        pkgs = import nixpkgs { inherit localSystem config overlays; };
-      in
-      rec {
-        packages = pkgs;
+        legacyPackages = pkgs;
 
         devShells = {
-          default = devShells.everything;
+          default = config.devShells.everything;
 
-          everything = pkgs.mkShell {
-            packages = [ pkgs.commonPackages ];
-          };
+          everything =
+            pkgs.mkShell {
+              packages = [ pkgs.commonPackages ];
+            };
         };
-      }
-    );
+      };
+    };
 
   nixConfig = {
     extra-substituters = "https://nix-community.cachix.org https://evanrelf.cachix.org";
