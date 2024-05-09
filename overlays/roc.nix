4f02627828474d13edcf332eef2e344102d84901
Evan Relf <evan@evanrelf.com>
2024-05-09 00:46:24 -0700

    Add broken derivation for `hello-roc`

--- overlays/roc.nix
+++ overlays/roc.nix
@@ -2,5 +2,16 @@ final: prev:
 
 {
   hello-roc =
-    null;
+    # TODO: How should dependencies be downloaded? Fixed output derivation
+    # running `roc check` and dumping cached dependencies into `$out`, for the
+    # final derivation to consume? `crane` for Rust could be a good reference.
+    final.stdenv.mkDerivation {
+      name = "hello-roc";
+      src = ../src/roc/hello;
+      nativeBuildInputs = [ final.roc ];
+      buildPhase = ''
+        mkdir -p $out/bin/
+        roc build --optimize --output $out/bin/hello
+      '';
+    };
 }
