2fff6be0672cf11baa78e12c7f437cf917e117d6
Evan Relf <evan@evanrelf.com>
2024-08-27 20:46:21 -0700

    Use less magical `typer` code in Python hello

--- src/python/hello.py
+++ src/python/hello.py
@@ -11,9 +11,13 @@ import typer
 from typing_extensions import Annotated
 
 
+app = typer.Typer()
+
+
+@app.command()
 def main(name: Annotated[str, typer.Argument()] = "world"):
     print(f"Hello, {name}!")
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    app()
