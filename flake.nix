29d27c55b4e86442b74de435f49d632450ac2156
Evan Relf <evan@evanrelf.com>
2024-09-17 14:00:30 -0700

    Update flake inputs

--- flake.nix
+++ flake.nix
@@ -2,10 +2,7 @@
   description = "dotfiles";
 
   inputs = {
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    crane.url = "github:ipetkov/crane";
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
