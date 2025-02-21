389a5fed400c5f665cbc3d6609ea2f8b7b4a6f24
Evan Relf <evan@evanrelf.com>
2025-02-21 11:40:36 -0800

    fish: Add file preview to `hsgoto` and `rsgoto`
    This code sucks but it works.

--- configs/fish/.config/fish/functions/rsgoto.fish
+++ configs/fish/.config/fish/functions/rsgoto.fish
@@ -1,22 +1,37 @@
 function rsgoto
-    set --local matches (mktemp)
-
-    gum spin --title "Searching..." -- \
-        rg \
-        --type rust \
-        --multiline \
-        "\b(?:fn|static|const|let(?:\s+\bmut\b)?)\s+\b(?:$argv[1])\b|\b(?:type|struct|enum|union|trait(?:<.+>\s+)?)\s+\b(?:$argv[1])\b" \
-        --line-number \
-        $argv[2..-1] >"$matches"
-
-    set --local count (wc -l < "$matches")
-
-    if test "$count" -eq 0
-        return 1
-    else
-        set --local match (cat "$matches" | eval "fzf $FZF_DEFAULT_OPTS -1" | cut -d ':' -f 1-2)
-        if test -n "$match"
-            "$EDITOR" "$match"
-        end
+    set --local fzf_opts
+    # TODO: This sucks
+    for opt in (echo "$FZF_DEFAULT_OPTS" | tr ' ' '\n')
+        set --append fzf_opts $opt
     end
+    set --local match (\
+        rg \
+            --type rust \
+            --multiline \
+            "\b(?:fn|static|const|let(?:\s+\bmut\b)?)\s+\b(?:$argv[1])\b|\b(?:type|struct|enum|union|trait(?:<.+>\s+)?)\s+\b(?:$argv[1])\b" \
+            --line-number \
+            $argv[2..-1] \
+        | cut \
+            -d ':' \
+            -f 1-2 \
+        | fzf $fzf_opts \
+            --exit-0 \
+            --select-1 \
+            --preview '
+                set -l file (echo {} | cut -d ":" -f 1)
+                set -l line (echo {} | cut -d ":" -f 2)
+                awk "
+                    # NR >= $line && /^\s/ {
+                    NR >= $line {
+                        print;
+                        count++;
+                        if (count == $FZF_PREVIEW_LINES) exit;
+                    }
+                    # NR >= $line && !/^\s/ {
+                    #     exit;
+                    # }
+                " $file
+            ' \
+    )
+    test -n "$match" && "$EDITOR" "$match"
 end
