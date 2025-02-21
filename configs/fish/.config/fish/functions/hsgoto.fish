faf0f778641f0ec6da4a38d54a9ece62d73fa8d6
Evan Relf <evan@evanrelf.com>
2025-02-21 15:45:02 -0800

    fish: Speed up `{hs,rs}goto`, add match and color

--- configs/fish/.config/fish/functions/hsgoto.fish
+++ configs/fish/.config/fish/functions/hsgoto.fish
@@ -1,37 +1,36 @@
 function hsgoto
-    set --local fzf_opts
-    # TODO: This sucks
-    for opt in (echo "$FZF_DEFAULT_OPTS" | tr ' ' '\n')
-        set --append fzf_opts $opt
-    end
     set --local match (\
         rg \
             --type haskell \
             --multiline \
-            "^ *\b(?:$argv[1])\b\s+::|^ *\b(?:type|type\s+family|newtype|data|class)\b.*? +\b(?:$argv[1])\b" \
-            --line-number \
+            "^ *\b(?<term>$argv[1])\b\s+::|^ *\b(?:type|type\s+family|newtype|data|class)\b.*? +\b(?<type>$argv[1])\b" \
+            --replace '$term$type' \
+            --only-matching \
+            --column \
+            --color always \
+            --colors 'path:none' \
+            --colors 'line:none' \
+            --colors 'column:none' \
+            --colors 'match:fg:red' \
+            --line-buffered \
             $argv[2..-1] \
-        | cut \
-            -d ':' \
-            -f 1-2 \
-        | fzf $fzf_opts \
+        | fzf \
+            (string split " " -- $FZF_DEFAULT_OPTS) \
+            --ansi \
             --exit-0 \
             --select-1 \
             --preview '
                 set -l file (echo {} | cut -d ":" -f 1)
                 set -l line (echo {} | cut -d ":" -f 2)
                 awk "
-                    # NR >= $line && /^\s/ {
                     NR >= $line {
                         print;
                         count++;
                         if (count == $FZF_PREVIEW_LINES) exit;
                     }
-                    # NR >= $line && !/^\s/ {
-                    #     exit;
-                    # }
                 " $file
             ' \
+        | cut -d ':' -f 1-3
     )
     test -n "$match" && "$EDITOR" "$match"
 end
--- configs/fish/.config/fish/functions/rsgoto.fish
+++ configs/fish/.config/fish/functions/rsgoto.fish
@@ -1,37 +1,36 @@
 function rsgoto
-    set --local fzf_opts
-    # TODO: This sucks
-    for opt in (echo "$FZF_DEFAULT_OPTS" | tr ' ' '\n')
-        set --append fzf_opts $opt
-    end
     set --local match (\
         rg \
             --type rust \
             --multiline \
-            "\b(?:fn|static|const|let(?:\s+\bmut\b)?)\s+\b(?:$argv[1])\b|\b(?:type|struct|enum|union|trait(?:<.+>\s+)?)\s+\b(?:$argv[1])\b" \
-            --line-number \
+            "\b(?:fn|static|const|let(?:\s+\bmut\b)?)\s+\b(?<term>$argv[1])\b|\b(?:type|struct|enum|union|trait(?:<.+>\s+)?)\s+\b(?<type>$argv[1])\b" \
+            --replace '$term$type' \
+            --only-matching \
+            --column \
+            --color always \
+            --colors 'path:none' \
+            --colors 'line:none' \
+            --colors 'column:none' \
+            --colors 'match:fg:red' \
+            --line-buffered \
             $argv[2..-1] \
-        | cut \
-            -d ':' \
-            -f 1-2 \
-        | fzf $fzf_opts \
+        | fzf \
+            (string split " " -- $FZF_DEFAULT_OPTS) \
+            --ansi \
             --exit-0 \
             --select-1 \
             --preview '
                 set -l file (echo {} | cut -d ":" -f 1)
                 set -l line (echo {} | cut -d ":" -f 2)
                 awk "
-                    # NR >= $line && /^\s/ {
                     NR >= $line {
                         print;
                         count++;
                         if (count == $FZF_PREVIEW_LINES) exit;
                     }
-                    # NR >= $line && !/^\s/ {
-                    #     exit;
-                    # }
                 " $file
             ' \
+        | cut -d ':' -f 1-3
     )
     test -n "$match" && "$EDITOR" "$match"
 end
