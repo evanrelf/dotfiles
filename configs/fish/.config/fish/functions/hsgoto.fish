db8eb9534e27da3481f23fd7443df8189a8da2fd
Evan Relf <evan@evanrelf.com>
2025-03-19 11:55:01 -0700

    fish: Move more `*goto` logic into `fzf`

--- configs/fish/.config/fish/functions/hsgoto.fish
+++ configs/fish/.config/fish/functions/hsgoto.fish
@@ -1,39 +1,35 @@
 function hsgoto
-    set --local match (\
-        rg \
-            --type haskell \
-            --multiline \
-            "^ *\b(?<term>$argv[1])\b\s+::|^ *\b(?:type|type\s+family|newtype|data|class)\b.*? +\b(?<type>$argv[1])\b" \
-            --replace '$term$type' \
-            --only-matching \
-            --column \
-            --color always \
-            --colors 'path:none' \
-            --colors 'line:none' \
-            --colors 'column:none' \
-            --colors 'match:fg:red' \
-            --line-buffered \
-            $argv[2..-1] \
+    rg \
+        --type haskell \
+        --multiline \
+        "^ *\b(?<term>$argv[1])\b\s+::|^ *\b(?:type|type\s+family|newtype|data|class)\b.*? +\b(?<type>$argv[1])\b" \
+        --replace '$term$type' \
+        --only-matching \
+        --column \
+        --color always \
+        --colors 'path:none' \
+        --colors 'line:none' \
+        --colors 'column:none' \
+        --colors 'match:fg:red' \
+        --line-buffered \
+        $argv[2..-1] \
         | fzf \
-            (string split " " -- $FZF_DEFAULT_OPTS) \
-            --ansi \
-            --exit-0 \
-            --select-1 \
-            --preview '
-                set -l file (echo {} | cut -d ":" -f 1)
-                set -l line (echo {} | cut -d ":" -f 2)
-                awk "
-                    NR >= $line {
-                        print;
-                        count++;
-                        if (count == $FZF_PREVIEW_LINES) exit;
-                    }
-                " $file
-            ' \
-        | cut -d ':' -f 1-3
-    )
-    if test -n "$match"
-        echo "$match"
-        "$EDITOR" "$match"
-    end
+        (string split " " -- $FZF_DEFAULT_OPTS) \
+        --ansi \
+        --exit-0 \
+        --delimiter ':' \
+        --with-nth '1,2,4' \
+        --nth '1,3' \
+        --preview '
+            set -l file {1}
+            set -l line {2}
+            awk "
+                NR >= $line {
+                    print;
+                    count++;
+                    if (count == $FZF_PREVIEW_LINES) exit;
+                }
+            " $file
+        ' \
+        --bind 'one,enter:become(echo {1..3}; $EDITOR {1..3})'
 end
--- configs/fish/.config/fish/functions/rsgoto.fish
+++ configs/fish/.config/fish/functions/rsgoto.fish
@@ -1,39 +1,35 @@
 function rsgoto
-    set --local match (\
-        rg \
-            --type rust \
-            --multiline \
-            "\b(?:fn|static|const|let(?:\s+\bmut\b)?)\s+\b(?<term>$argv[1])\b|\b(?:type|struct|enum|union|trait(?:<.+>\s+)?)\s+\b(?<type>$argv[1])\b" \
-            --replace '$term$type' \
-            --only-matching \
-            --column \
-            --color always \
-            --colors 'path:none' \
-            --colors 'line:none' \
-            --colors 'column:none' \
-            --colors 'match:fg:red' \
-            --line-buffered \
-            $argv[2..-1] \
+    rg \
+        --type rust \
+        --multiline \
+        "\b(?:fn|static|const|let(?:\s+\bmut\b)?)\s+\b(?<term>$argv[1])\b|\b(?:type|struct|enum|union|trait(?:<.+>\s+)?)\s+\b(?<type>$argv[1])\b" \
+        --replace '$term$type' \
+        --only-matching \
+        --column \
+        --color always \
+        --colors 'path:none' \
+        --colors 'line:none' \
+        --colors 'column:none' \
+        --colors 'match:fg:red' \
+        --line-buffered \
+        $argv[2..-1] \
         | fzf \
-            (string split " " -- $FZF_DEFAULT_OPTS) \
-            --ansi \
-            --exit-0 \
-            --select-1 \
-            --preview '
-                set -l file (echo {} | cut -d ":" -f 1)
-                set -l line (echo {} | cut -d ":" -f 2)
-                awk "
-                    NR >= $line {
-                        print;
-                        count++;
-                        if (count == $FZF_PREVIEW_LINES) exit;
-                    }
-                " $file
-            ' \
-        | cut -d ':' -f 1-3
-    )
-    if test -n "$match"
-        echo "$match"
-        "$EDITOR" "$match"
-    end
+        (string split " " -- $FZF_DEFAULT_OPTS) \
+        --ansi \
+        --exit-0 \
+        --delimiter ':' \
+        --with-nth '1,2,4' \
+        --nth '1,3' \
+        --preview '
+            set -l file {1}
+            set -l line {2}
+            awk "
+                NR >= $line {
+                    print;
+                    count++;
+                    if (count == $FZF_PREVIEW_LINES) exit;
+                }
+            " $file
+        ' \
+        --bind 'one,enter:become(echo {1..3}; $EDITOR {1..3})'
 end
