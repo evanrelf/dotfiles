b19173fa13815e4c907a137d08d939204d528e62
Evan Relf <evan@evanrelf.com>
2025-11-17 16:32:46 -0800

    fish: Add completions for `pueue`

--- configs/fish/.config/fish/config.fish
+++ configs/fish/.config/fish/config.fish
@@ -52,6 +52,9 @@ end
 if command -q jj
     jj util completion fish | source
 end
+if command -q pueue
+    pueue completions fish | source
+end
 function fish_user_key_bindings
     bind ctrl-z 'fg 2>/dev/null; commandline -f repaint'
 end
