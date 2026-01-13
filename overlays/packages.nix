cd49ba60ac1b8d813dff8b1d31c786af7716ea96
Evan Relf <evan@evanrelf.com>
2026-01-13 11:09:45 -0800

    Package `magika` using pre-built binaries

--- overlays/packages.nix
+++ overlays/packages.nix
@@ -62,6 +62,35 @@ in
   hsl =
     rust { name = "hsl"; };
 
+  magika-cli =
+    final.stdenvNoCC.mkDerivation rec {
+      pname = "magika-cli";
+      version = "1.0.1";
+      src = {
+        aarch64-darwin = final.fetchurl {
+          url = "https://github.com/google/magika/releases/download/cli/v${version}/magika-aarch64-apple-darwin.tar.xz";
+          hash = "sha256-mAaHvzRTa4I5ie12d7B6L7TnhkxJs2k1kfjylPOhslM=";
+        };
+        aarch64-linux = final.fetchurl {
+          url = "https://github.com/google/magika/releases/download/cli/v${version}/magika-aarch64-unknown-linux-gnu.tar.xz";
+          sha256 = "sha256-L+cfuvcqFaNk/2dM8S7mN8QPnIzTfvj0Y4uc2GORg9g=";
+        };
+        x86_64-linux = final.fetchurl {
+          url = "https://github.com/google/magika/releases/download/cli/v${version}/magika-x86_64-unknown-linux-gnu.tar.xz";
+          sha256 = "sha256-af7nFv8NL7YcImloMzpgAMfRXJdIzC0l4jobB9PiifA=";
+        };
+      }.${final.stdenv.hostPlatform.system};
+      nativeBuildInputs = final.lib.optionals final.stdenv.isLinux [
+        final.autoPatchelfHook
+      ];
+      installPhase = ''
+        runHook preInstall
+        mkdir -p $out/bin
+        install -m755 magika $out/bin/magika
+        runHook postInstall
+      '';
+    };
+
   # kakoune-unwrapped =
   #   prev.kakoune-unwrapped.overrideAttrs (attrs: rec {
   #     version = final.inputs.kakoune.shortRev;
