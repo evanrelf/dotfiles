be73419dd3563cd9b2d7fd80c869e6f40ff6770b
Evan Relf <evan@evanrelf.com>
2026-01-16 20:56:08 -0800

    iris: Disable `system.copySystemConfiguration`
    Not compatible with flakes.

--- modules/nixos/machines/iris/configuration.nix
+++ modules/nixos/machines/iris/configuration.nix
@@ -74,11 +74,6 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  system.copySystemConfiguration = true;
-
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
