6f2c050c508a0889395cb4bffb4a7ab5e2e91900
Evan Relf <evan@evanrelf.com>
2026-02-01 23:14:04 -0800

    Revert "Add `container`"
    This reverts commit 5f4d64b73694ddff34d94c64554146c62fbea920.

--- modules/home/machines/ultraviolet.nix
+++ modules/home/machines/ultraviolet.nix
@@ -1,13 +1,8 @@
-{ config, pkgs, ... }:
+{ config, ... }:
 
 {
   imports = [
     ../common.nix
-  ];
-
-  home.packages = with pkgs; [
-    # TODO: Move to common Darwin packages once `pearl` upgrades to macOS Tahoe.
-    container
   ];
 
   home.stateVersion = "22.11";
