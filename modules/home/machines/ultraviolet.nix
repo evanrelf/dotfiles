5f4d64b73694ddff34d94c64554146c62fbea920
Evan Relf <evan@evanrelf.com>
2026-02-01 22:41:05 -0800

    Add `container`

--- modules/home/machines/ultraviolet.nix
+++ modules/home/machines/ultraviolet.nix
@@ -1,8 +1,13 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
   imports = [
     ../common.nix
+  ];
+
+  home.packages = with pkgs; [
+    # TODO: Move to common Darwin packages once `pearl` upgrades to macOS Tahoe.
+    container
   ];
 
   home.stateVersion = "22.11";
