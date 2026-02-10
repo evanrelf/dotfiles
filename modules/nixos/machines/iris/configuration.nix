f86b7a02a1f90a31edc140e3b8cc7fa0872d2a3b
Evan Relf <evan@evanrelf.com>
2026-02-09 22:35:21 -0800

    iris: Make Nix trust users in `wheel` group
    Stupid this isn't the default, considering being in `wheel` means you
    can make this very config change!

--- modules/nixos/machines/iris/configuration.nix
+++ modules/nixos/machines/iris/configuration.nix
@@ -178,6 +178,7 @@ in
 
   nix.package = pkgs.lixPackageSets.latest.lix;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.trusted-users = [ "@wheel" ];
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
