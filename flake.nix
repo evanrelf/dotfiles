e7f40c27b327664d112f02f9999b14ec4766b6b5
Evan Relf <evan@evanrelf.com>
2026-02-12 22:42:17 -0800

    Add substituter for `numtide/llm-agents.nix`

--- flake.nix
+++ flake.nix
@@ -80,4 +80,9 @@
         legacyPackages = pkgs;
       };
     };
+
+  nixConfig = {
+    extra-substituters = [ "https://cache.numtide.com" ];
+    extra-trusted-public-keys = [ "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g=" ];
+  };
 }
