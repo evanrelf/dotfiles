725232a7a42ce32e478b9826f2659a2a3a19fe38
Evan Relf <evan@evanrelf.com>
2026-03-25 19:53:47 -0700

    neovim: Enable Tree-sitter-based indentation

--- configs/neovim/.config/nvim/init.lua
+++ configs/neovim/.config/nvim/init.lua
@@ -56,5 +56,6 @@ vim.api.nvim_create_autocmd("FileType", {
   },
   callback = function()
     vim.treesitter.start()
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
   end,
 })
