17ce086482658a7170cd9cb57065bcf5459c310b
Evan Relf <evan@evanrelf.com>
2026-03-25 17:40:58 -0700

    pearl: Update known hashes for files

--- modules/flake/machines/pearl/default.nix
+++ modules/flake/machines/pearl/default.nix
@@ -25,9 +25,9 @@
     # For now, these hashes tell `nix-darwin` these modifications are okay to
     # overwrite. In the future, it would be better to clean up this hodgepodge of
     # Nix installs so this hack isn't necessary.
-    environment.etc."bashrc".knownSha256Hashes = [ "b67c9ea69b96f0ba89c575ddf9631d556d682ce1a3203cf0a458e368659f20ee" ];
+    environment.etc."bashrc".knownSha256Hashes = [ "fc15d82a792799f14431e643b35fd9f6b07b374ed0509ceb14052f05fe664b8a" ];
     environment.etc."zshrc".knownSha256Hashes = [ "27274e44b88a1174787f9a3d437d3387edc4f9aaaf40356054130797f5dc7912" ];
-    environment.etc."zshenv".knownSha256Hashes = [ "e7a5ba03dc84e14c8c9028888a4278c03d3f23e528b8424ab24d67aae93d59fa" ];
+    environment.etc."zshenv".knownSha256Hashes = [ "ac3d39febe5ffcd0f404dda281bea91c6a2ec9870896276989baa00fd2bb23a6" ];
 
     system.stateVersion = 6;
   };
