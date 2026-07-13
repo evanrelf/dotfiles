35dce07b9d731a6807c2ab83b14aa4dff6c5d79e
Evan Relf <evan@evanrelf.com>
2026-07-13 17:47:48 -0400

    Initial tweaks for Antithesis machine

---
+++ modules/flake/machines/evanrelf-desktop.nix
@@ -0,0 +1,24 @@
+{ config, inputs, withSystem, ... }:
+
+{
+  flake.homeConfigurations.evanrelf-desktop =
+    withSystem "x86_64-linux" ({ pkgs, ... }:
+      inputs.home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ config.flake.homeModules.evanrelf-desktop ];
+        extraSpecialArgs = { inherit inputs; };
+      }
+    );
+
+  flake.homeModules.evanrelf-desktop = { pkgs, ... }: {
+    imports = [
+      ../../home/common.nix
+    ];
+
+    home.packages = with pkgs; [
+      ghostty
+    ];
+
+    home.stateVersion = "26.05";
+  };
+}
