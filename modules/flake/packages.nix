78ad18164ec9e07c49b59644475448251b6b7057
Evan Relf <evan.relf@antithesis.com>
2026-08-20 13:58:16 -0400

    Drop unused `assertUpgrade` function
    It relied to `final: prev:` which isn't available anymore with the flake
    modules.

--- modules/flake/packages.nix
+++ modules/flake/packages.nix
@@ -3,18 +3,6 @@
 {
   perSystem = { pkgs, system, ... }:
     let
-      assertUpgrade = prevDrv: finalDrv:
-        let
-          older = builtins.compareVersions (prevDrv.version or "0") finalDrv.version < 0;
-          error = builtins.throw ''
-            '${finalDrv.pname}' override is outdated
-
-            Version from Nixpkgs:  ${prevDrv.version}
-            Version from dotfiles: ${finalDrv.version}
-          '';
-        in
-        assert older || error; finalDrv;
-
       gprefix = drv:
         pkgs.runCommandLocal "gprefix-${drv.name}" { } ''
           mkdir -p "$out/bin"
