9f2f033a12335b6e839c095031550a3cf9d20e0e
Evan Relf <evan@evanrelf.com>
2026-09-04 18:52:20 -0400

    fish: Fix incorrect colors in dark mode

--- configs/fish/.config/fish/conf.d/colors.fish
+++ configs/fish/.config/fish/conf.d/colors.fish
@@ -1,5 +1,5 @@
 set --global --export COLORTERM "$TERM"
-set --global fish_color_command black
+set --global fish_color_command normal
 set --global fish_color_keyword $fish_color_command
 set --global fish_color_param $fish_color_command
 set --global fish_color_option $fish_color_param
@@ -14,8 +14,8 @@ set --global fish_color_cancel brblack
 set --global fish_color_comment brblack --italics
 set --global fish_color_autosuggestion brblack
 if test -e "$__fish_config_dir/conf.d/plugin-fish-colored-man.fish"
-    set --global man_blink --reverse black
-    set --global man_bold --bold black
-    set --global man_standout black
-    set --global man_underline --underline black
+    set --global man_blink --reverse normal
+    set --global man_bold --bold normal
+    set --global man_standout normal
+    set --global man_underline --underline normal
 end
